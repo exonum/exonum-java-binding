a41a66db682e1a79cd8605bcabe9ce0482d02708
Stanislav Tkach <stanislav.tkach@xdev.re>
2017-07-06 17:09:14 +0300

    Use RWLock and HashMap instead of Mutex and BTreeMap

--- rust/src/utils/resource_manager/imp.rs
+++ rust/src/utils/resource_manager/imp.rs
@@ -1,18 +1,18 @@
 use std::any::TypeId;
-use std::sync::Mutex;
-use std::collections::BTreeMap;
+use std::sync::RwLock;
+use std::collections::HashMap;
 
 use utils::Handle;
 
 lazy_static! {
-    static ref HANDLES_MAP: Mutex<BTreeMap<Handle, TypeId>> = Mutex::new(BTreeMap::new());
+    static ref HANDLES_MAP: RwLock<HashMap<Handle, TypeId>> = RwLock::new(HashMap::new());
 }
 
 pub fn add_handle<T: 'static>(handle: Handle) {
     assert_ne!(handle, 0);
     assert!(
         HANDLES_MAP
-            .lock()
+            .write()
             .unwrap()
             .insert(handle, TypeId::of::<T>())
             .is_none(),
@@ -22,11 +22,11 @@ pub fn add_handle<T: 'static>(handle: Handle) {
 
 pub fn remove_handle<T: 'static>(handle: Handle) {
     check_handle::<T>(handle);
-    HANDLES_MAP.lock().unwrap().remove(&handle);
+    HANDLES_MAP.write().unwrap().remove(&handle);
 }
 
 pub fn check_handle<T: 'static>(handle: Handle) {
-    match HANDLES_MAP.lock().unwrap().get(&handle) {
+    match HANDLES_MAP.read().unwrap().get(&handle) {
         Some(type_id) => {
             assert_eq!(
                 type_id,
