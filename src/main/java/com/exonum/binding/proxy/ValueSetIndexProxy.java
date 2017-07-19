597aa694218546a6815ad0b3496449c110f4bd4f
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2017-07-19 13:29:42 +0300

    Add AbstractIndexProxy: a super class for all indices, containing code shared by all indices.

--- src/main/java/com/exonum/binding/proxy/ValueSetIndexProxy.java
+++ src/main/java/com/exonum/binding/proxy/ValueSetIndexProxy.java
@@ -26,14 +26,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  *
  * @see KeySetIndexProxy
  */
-public class ValueSetIndexProxy extends AbstractNativeProxy {
-
-  private final View dbView;
-
-  /**
-   * Needed to detect modifications of this set during iteration over other indices.
-   */
-  private final ViewModificationCounter modCounter;
+public class ValueSetIndexProxy extends AbstractIndexProxy {
 
   /**
    * Creates a new value set proxy.
@@ -46,9 +39,7 @@ public class ValueSetIndexProxy extends AbstractNativeProxy {
    * @throws NullPointerException if any argument is null
    */
   ValueSetIndexProxy(byte[] prefix, View view) {
-    super(nativeCreate(checkIndexPrefix(prefix), view.getNativeHandle()), true);
-    dbView = view;
-    modCounter = ViewModificationCounter.getInstance();
+    super(nativeCreate(checkIndexPrefix(prefix), view.getNativeHandle()), view);
   }
 
   /**
@@ -200,10 +191,6 @@ public class ValueSetIndexProxy extends AbstractNativeProxy {
   public void removeByHash(byte[] elementHash) {
     notifyModified();
     nativeRemoveByHash(getNativeHandle(), checkNotNull(elementHash));
-  }
-
-  private void notifyModified() {
-    modCounter.notifyModified(checkCanModify(dbView));
   }
 
   @Override
