c8f7655f31e78e3e79ee960b25b07f8237156eca
Stanislav Tkach <stanislav.tkach@xdev.re>
2017-08-22 17:17:00 +0300

    Fix formatting

--- exonum-java-binding-core/rust/src/storage/proof_list_index.rs
+++ exonum-java-binding-core/rust/src/storage/proof_list_index.rs
@@ -342,7 +342,7 @@ fn make_java_proof<'a>(env: &JNIEnv<'a>, proof: &ListProof<Value>) -> Result<JOb
 }
 
 // TODO: Remove attribute (https://github.com/rust-lang-nursery/rust-clippy/issues/1981).
-#[cfg_attr(feature="cargo-clippy", allow(ptr_arg))]
+#[cfg_attr(feature = "cargo-clippy", allow(ptr_arg))]
 fn make_java_proof_element<'a>(env: &JNIEnv<'a>, value: &Value) -> Result<JObject<'a>> {
     let value: JObject = env.byte_array_from_slice(value)?.into();
     env.new_object(
