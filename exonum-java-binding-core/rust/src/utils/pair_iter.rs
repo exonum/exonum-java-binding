cafce62f6e7f7b31b107a4e4f72be85aae37d382
Stanislav Tkach <stanislav.tkach@xdev.re>
2017-09-01 15:00:02 +0300

    Apply review comments

--- exonum-java-binding-core/rust/src/utils/pair_iter.rs
+++ exonum-java-binding-core/rust/src/utils/pair_iter.rs
@@ -4,17 +4,18 @@ use jni::errors::Result;
 
 use std::mem;
 
-pub struct PairIter<InnerIter> {
+pub struct PairIter<InnerIter: Iterator> {
     pub iter: InnerIter,
     pub element_class: GlobalRef,
     pub constructor_id: JMethodID<'static>,
 }
 
-impl<InnerIter> PairIter<InnerIter> {
+impl<InnerIter: Iterator> PairIter<InnerIter> {
     pub fn new(env: &JNIEnv, iter: InnerIter, class_name: &str) -> Result<Self> {
         let class = env.find_class(class_name)?;
         let element_class = env.new_global_ref(class.into())?;
-        let id = env.get_method_id(class_name, "<init>", "([B[B)V")?;
+        let signature = "([B[B)V";
+        let id = env.get_method_id(class_name, "<init>", signature)?;
         Ok(PairIter {
             iter,
             element_class,
