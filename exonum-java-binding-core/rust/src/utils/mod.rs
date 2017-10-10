b96e62f5c71d94c6b82c218fe6d513ce592dfe10
Stanislav Tkach <stanislav.tkach@gmail.com>
2017-10-10 12:40:30 +0300

    Update rust-jni version (#151)

--- exonum-java-binding-core/rust/src/utils/mod.rs
+++ exonum-java-binding-core/rust/src/utils/mod.rs
@@ -6,10 +6,8 @@ mod exception;
 mod handle;
 mod resource_manager;
 mod pair_iter;
-mod local_ref;
 
 pub use self::conversion::{convert_to_hash, convert_hash};
 pub use self::exception::{unwrap_exc_or, unwrap_exc_or_default};
 pub use self::handle::{Handle, to_handle, cast_handle, drop_handle};
 pub use self::pair_iter::PairIter;
-pub use self::local_ref::AutoLocalRef;
