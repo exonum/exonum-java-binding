f19a8a3f1a6b95866d80481647f905aaf305ea5b
Dmitry Timofeev <dmitry-timofeev@users.noreply.github.com>
2017-11-20 18:35:41 +0200

    Update the API docs of the indices [EEN-4]: (#176)
    Use the following plan:
    
    <Index description>
    
    <When to use>
    
    <Restrictions on elements (elements, keys, values)>
    
    <What a destructive operations are>
    
    <Nullability: non-null by default>
    
    <Thread safety>
    
    <Must be closed>
    
    @see View (that introduces the reader to read-only/rw views
    with references to their implementations)

--- exonum-java-binding-core/src/main/java/com/exonum/binding/storage/indices/MapIndexProxy.java
+++ exonum-java-binding-core/src/main/java/com/exonum/binding/storage/indices/MapIndexProxy.java
@@ -10,21 +10,27 @@ import com.exonum.binding.storage.database.View;
  * A MapIndex is an index that maps keys to values. A map cannot contain duplicate keys;
  * each key corresponds to at most one value.
  *
+ * <p>The map implementation does not permit null keys and values.
+ *
  * <p>The "destructive" methods of the map, i.e., the one that change the map contents,
  * are specified to throw {@link UnsupportedOperationException} if
  * the map has been created with a read-only database view.
  *
- * <p>The map implementation does not permit null keys and values.
+ * <p>All method arguments are non-null by default.
+ *
+ * <p>This class is not thread-safe and its instances shall not be shared between threads.
  *
  * <p>As any native proxy, the map <em>must be closed</em> when no longer needed.
  * Subsequent use of the closed map is prohibited and will result in {@link IllegalStateException}.
+ *
+ * @see View
  */
 public class MapIndexProxy extends AbstractIndexProxy implements MapIndex {
 
   /**
    * Creates a new MapIndexProxy.
    *
-   * @param name a unique alphanumeric identifier of this map in the underlying storage:
+   * @param name a unique alphanumeric non-empty identifier of this map in the underlying storage:
    *             [a-zA-Z0-9_]
    * @param view a database view. Must be valid.
    *             If a view is read-only, "destructive" operations are not permitted.
--- exonum-java-binding-core/src/main/java/com/exonum/binding/storage/indices/ValueSetIndexProxy.java
+++ exonum-java-binding-core/src/main/java/com/exonum/binding/storage/indices/ValueSetIndexProxy.java
@@ -22,17 +22,22 @@ import com.exonum.binding.storage.database.View;
  * are specified to throw {@link UnsupportedOperationException} if the set has been created with
  * a read-only database view.
  *
+ * <p>All method arguments are non-null by default.
+ *
+ * <p>This class is not thread-safe and and its instances shall not be shared between threads.
+ *
  * <p>As any native proxy, the set <em>must be closed</em> when no longer needed.
  * Subsequent use of the closed set is prohibited and will result in {@link IllegalStateException}.
  *
  * @see KeySetIndexProxy
+ * @see View
  */
 public class ValueSetIndexProxy extends AbstractIndexProxy {
 
   /**
    * Creates a new value set proxy.
    *
-   * @param name a unique alphanumeric identifier of this set in the underlying storage:
+   * @param name a unique alphanumeric non-empty identifier of this set in the underlying storage:
    *             [a-zA-Z0-9_]
    * @param view a database view. Must be valid. If a view is read-only,
    *             "destructive" operations are not permitted.
