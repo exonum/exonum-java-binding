aa4ec9a99612a6721f85d0927fa6480ec6582424
Alexander Irbis <irbis.labs@gmail.com>
2018-01-18 15:25:31 +0200

    Reimplemented saving of handle

--- exonum-java-binding-core/rust/src/storage/db.rs
+++ exonum-java-binding-core/rust/src/storage/db.rs
@@ -2,6 +2,9 @@ use jni::JNIEnv;
 use jni::objects::JClass;
 use exonum::storage::{Snapshot, Fork};
 
+use std::collections::HashMap;
+use std::sync::RwLock;
+
 use utils::{self, Handle};
 
 pub(crate) type Key = Vec<u8>;
@@ -14,39 +17,68 @@ pub(crate) enum View {
     Fork(&'static mut Fork),
 }
 
-struct Pair (Handle, usize);
+lazy_static! {
+    // Raw pointers are used as identifiers
+    static ref VIEW_MAP: RwLock<HashMap<usize, Handle>> = RwLock::new(HashMap::new());
+}
+
+struct Pair (usize, usize);
 
 impl View {
     pub fn from_owned_snapshot(snapshot: Box<Snapshot>) -> Self {
+        // Clone a fat reference
         let snapshot_ref = unsafe { ::std::mem::transmute(&*snapshot) };
+        let view = View::Snapshot(snapshot_ref);
         let handle = utils::to_handle::<Box<Snapshot>>(snapshot);
-        let handle_2 = unsafe { ::std::mem::transmute_copy::<_, Pair>(&snapshot_ref) }.0;
-        assert_eq!(handle, handle_2);
-        View::Snapshot(snapshot_ref)
+        view.save_handle(handle);
+        view
     }
 
     pub fn from_owned_fork(fork: Fork) -> Self {
-        let fork = utils::to_handle::<Fork>(fork);
-        let fork = unsafe { ::std::mem::transmute(fork) };
-        View::Fork(fork)
+        let handle = utils::to_handle::<Fork>(fork);
+        let fork = unsafe { ::std::mem::transmute_copy(&handle) };
+        let view = View::Fork(fork);
+        view.save_handle(handle);
+        view
     }
 
     #[cfg_attr(feature="cargo-clippy", allow(needless_borrow))]
     fn drop_owned(env: &JNIEnv, view: &View) {
-        match *view {
-            View::Snapshot(ref pair) => {
-                let handle = unsafe { ::std::mem::transmute_copy::<_, Pair>(pair) }.0;
-                if utils::has_handle(handle) {
-                    utils::drop_handle::<Box<Snapshot>>(env, handle);
-                }
-            }
-            View::Fork(ref handle) => {
-                let handle = unsafe { ::std::mem::transmute_copy(handle) };
-                if utils::has_handle(handle) {
-                    utils::drop_handle::<Fork>(env, handle);
-                }
-            }
-        };
+        if let Some(handle) = view.remove_handle() {
+            match *view {
+                View::Snapshot(_) => utils::drop_handle::<Box<Snapshot>>(env, handle),
+                View::Fork(_) => utils::drop_handle::<Fork>(env, handle),
+            };
+        }
+    }
+
+    fn id(&self) -> usize {
+        match *self {
+            View::Snapshot(ref snapshot) => unsafe { ::std::mem::transmute_copy::<_, Pair>(snapshot) }.0,
+            View::Fork(ref fork) => unsafe { ::std::mem::transmute_copy(fork) },
+        }
+    }
+
+    fn save_handle(&self, handle: Handle) {
+        let id = self.id();
+        assert_ne!(id, 0);
+        assert!(
+            VIEW_MAP
+                .write()
+                .expect("Unable to obtain write-lock")
+                .insert(id, handle)
+                .is_none(),
+            "Trying to register the same id for the second time: {:X}",
+            id
+        );
+    }
+
+    fn remove_handle(&self) -> Option<Handle> {
+        let id = self.id();
+        VIEW_MAP
+            .write()
+            .expect("Unable to obtain write-lock")
+            .remove(&id)
     }
 }
 
