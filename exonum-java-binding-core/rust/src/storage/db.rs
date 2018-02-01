a13ea04fefbb48ca212749523fce7925d68e80b8
Alexander Irbis <irbis.labs@gmail.com>
2018-02-01 17:24:04 +0200

    Improved the documentation and comments

--- exonum-java-binding-core/rust/src/storage/db.rs
+++ exonum-java-binding-core/rust/src/storage/db.rs
@@ -7,20 +7,26 @@ use utils::{self, Handle};
 pub(crate) type Key = Vec<u8>;
 pub(crate) type Value = Vec<u8>;
 
-/// Raw pointer to the `View` is returned to the java side, so in rust functions that take back
-/// `Snapshot` or `Fork` it will be possible to distinguish them.
+
+/// A `View` is a wrapper for `Snapshot` or `Fork`, which makes it possible to distinguish them
+/// on the rust side, and transfer them as a raw pointer via the java side.
 ///
-/// Why there is a separate reference?
-/// Full `Box<Snapshot>`/`Box<Fork>` type may be saved as owned value to be able drop it in the end.
-/// Short `&Snapshot`/`&Fork` type is provided by the core and we have to make `ViewRef` compatible.
+/// Additionally, the `View` type is used in two scenarios now:
+/// - it holds owned value of `Fork` or `Snapshot` and destroys it in the end,
+///    in the case when the java side creates and owns it;
+/// - it just holds a reference, when one is provided from the rust side.
 ///
-/// Why `Fork` is boxed?
-/// This is needed to make sure that it will not be moved after creating a reference.
-/// `View` itself will be moved from the stack to the heap in order to be converted in `Handle`.
+/// For both scenarios there is only a reference needed for underlying storage indexes, so
+/// an optional `owned` part is private.
+///
+/// For storage API we need a reference, so we create it to the owned part. But since there is no
+/// way in Rust to make a `View` value not movable. Furthermore, it have to be moved from the stack
+/// to the heap in order to be converted into `Handle` for the java side. So a `Fork` value
+/// should be placed in the heap to prevent its movement after creating a reference to it.
 
 pub(crate) struct View {
-    // The `owned` field is only needed for the drop stage,
-    // so `Box<Fork/Snapshot>` will be dropped when an instance of `View` will leave the scope.
+    // The `owned` field is used, it is not a fake, but its value only needed for the drop stage,
+    // so `Box<Fork>`/`Box<Snapshot>` will be dropped when an instance of `View` leaves the scope.
     #[allow(dead_code)]
     owned: Option<ViewOwned>,
     reference: ViewRef,
@@ -38,7 +44,7 @@ pub(crate) enum ViewRef {
 
 impl View {
     pub fn from_owned_snapshot(snapshot: Box<Snapshot>) -> Self {
-        // Make a reference to an owned data
+        // Make a "self-reference" to a value in the `owned` field
         let snapshot_ref = unsafe { &*(&*snapshot as *const Snapshot) };
         let reference = ViewRef::Snapshot(snapshot_ref);
         let owned = Some(ViewOwned::Snapshot(snapshot));
@@ -46,9 +52,9 @@ impl View {
     }
 
     pub fn from_owned_fork(fork: Fork) -> Self {
-        // Box `Fork` to make sure it will not be moved later
+        // Box a `Fork` value to make sure it will not be moved later and will not break a reference
         let mut fork = Box::new(fork);
-        // Make a reference to an owned data
+        // Make a "self-reference" to a value in the `owned` field
         let fork_ref = unsafe { &mut *(&mut *fork as *mut Fork) };
         let reference = ViewRef::Fork(fork_ref);
         let owned = Some(ViewOwned::Fork(fork));
@@ -58,7 +64,7 @@ impl View {
     // Will be removed in #ECR-242
     #[allow(dead_code)]
     pub fn from_ref_snapshot(snapshot: &Snapshot) -> Self {
-        // Make a reference `'static`
+        // Make a provided reference `'static`
         let snapshot_ref = unsafe { &*(&*snapshot as *const Snapshot) };
         View {
             owned: None,
@@ -69,7 +75,7 @@ impl View {
     // Will be removed in #ECR-242
     #[allow(dead_code)]
     pub fn from_ref_fork(fork: &mut Fork) -> Self {
-        // Make a reference `'static`
+        // Make a provided reference `'static`
         let fork_ref = unsafe { &mut *(fork as *mut Fork) };
         View {
             owned: None,
