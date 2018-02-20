582e8beede0fe9dc399e4c355e59e39a63e22c25
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-02-20 13:20:58 +0300

    Refactor method order and restrict check_read

--- exonum-java-binding-core/rust/src/storage/indexes_metadata.rs
+++ exonum-java-binding-core/rust/src/storage/indexes_metadata.rs
@@ -56,26 +56,11 @@ impl From<u8> for TableType {
     }
 }
 
-fn indexes_metadata<T>(view: T) -> MapIndex<T, String, IndexMetadata> {
-    MapIndex::new(SHADOW_TABLE_NAME, view)
-}
-
-fn assert_table_type(name: &str, table_type: TableType, stored_type: TableType) {
-    assert_eq!(
-        stored_type,
-        table_type,
-        "Attempt to access index '{}' of type {:?}, while said index was initially created with type {:?}",
-        name,
-        table_type,
-        stored_type
-    );
-}
-
-pub fn check_read<T: AsRef<Snapshot>>(name: &str, table_type: TableType, view: T) {
+pub fn check_read(name: &str, table_type: TableType, view: &Snapshot) {
     let metadata = indexes_metadata(view);
     if let Some(value) = metadata.get(&name.to_owned()) {
         let stored_type = TableType::from(value.table_type());
-        assert_table_type(name, table_type, stored_type);
+        check_table_type(name, table_type, stored_type);
     }
 }
 
@@ -86,10 +71,25 @@ pub fn check_write(name: &str, table_type: TableType, view: &mut Fork) {
     let mut metadata = indexes_metadata(view);
     if let Some(value) = metadata.get(&name.to_owned()) {
         let stored_type = TableType::from(value.table_type());
-        assert_table_type(name, table_type, stored_type);
+        check_table_type(name, table_type, stored_type);
     } else {
         metadata.put(&name.to_owned(), IndexMetadata::new(table_type.into()));
     }
+}
+
+fn indexes_metadata<T>(view: T) -> MapIndex<T, String, IndexMetadata> {
+    MapIndex::new(SHADOW_TABLE_NAME, view)
+}
+
+fn check_table_type(name: &str, table_type: TableType, stored_type: TableType) {
+    assert_eq!(
+        stored_type,
+        table_type,
+        "Attempt to access index '{}' of type {:?}, while said index was initially created with type {:?}",
+        name,
+        table_type,
+        stored_type
+    );
 }
 
 #[cfg(test)]
