abebfad9f807737d8be55f670713d209e5c1540f
Alexander Irbis <irbis.labs@gmail.com>
2018-03-09 20:09:07 +0200

    Added and enhanced exception check tests.

--- exonum-java-binding-core/rust/integration_tests/tests/utils_errors.rs
+++ exonum-java-binding-core/rust/integration_tests/tests/utils_errors.rs
@@ -7,12 +7,14 @@ mod util;
 use java_bindings::{DumbExecutor, Executor};
 use java_bindings::jni::{JNIEnv, JavaVM};
 use java_bindings::jni::errors::{ErrorKind, Result as JNIResult};
-use java_bindings::utils::panic_on_exception;
+use java_bindings::utils::{check_error_on_exception, panic_on_exception};
 
 use std::sync::Arc;
 
 use util::create_vm_for_tests;
 
+const ERROR_CLASS: &str = "java/lang/Error";
+const OOM_ERROR_CLASS: &str = "java/lang/OutOfMemoryError";
 const EXCEPTION_CLASS: &str = "java/lang/Exception";
 const ARITHMETIC_EXCEPTION_CLASS: &str = "java/lang/ArithmeticException";
 
@@ -23,7 +25,7 @@ lazy_static! {
 
 #[test]
 #[should_panic(expected="Java exception: java.lang.Exception")]
-fn catch_exact_class() {
+fn panic_on_exception_catch_exact_class() {
     EXECUTOR.with_attached(|env: &JNIEnv| {
         Ok(panic_on_exception(env, throw(env, EXCEPTION_CLASS)))
     })
@@ -32,7 +34,7 @@ fn catch_exact_class() {
 
 #[test]
 #[should_panic(expected="Java exception: java.lang.ArithmeticException")]
-fn catch_subclass() {
+fn panic_on_exception_catch_subclass() {
     EXECUTOR.with_attached(|env: &JNIEnv| {
         Ok(panic_on_exception(env, throw(env, ARITHMETIC_EXCEPTION_CLASS)))
     })
@@ -40,14 +42,82 @@ fn catch_subclass() {
 }
 
 #[test]
-fn dont_catch_without_exception() {
+#[should_panic(expected="JNI error: ")]
+fn panic_on_exception_catch_jni_error() {
+    EXECUTOR.with_attached(|env: &JNIEnv| {
+        Ok(panic_on_exception(env, make_jni_error()))
+    })
+        .unwrap();
+}
+
+#[test]
+fn panic_on_exception_dont_catch_good_result() {
     EXECUTOR.with_attached(|env: &JNIEnv| {
         Ok(panic_on_exception(env, Ok(())))
     })
         .unwrap();
 }
 
+#[test]
+#[should_panic(expected="Java exception: java.lang.Error")]
+fn check_error_on_exception_catch_javaerror_exact_class() {
+    EXECUTOR.with_attached(|env: &JNIEnv| {
+        Ok(check_error_on_exception(env, throw(env, ERROR_CLASS)))
+    })
+        .unwrap().unwrap();
+}
+
+#[test]
+#[should_panic(expected="Java exception: java.lang.OutOfMemoryError")]
+fn check_error_on_exception_catch_javaerror_subclass() {
+    EXECUTOR.with_attached(|env: &JNIEnv| {
+        Ok(check_error_on_exception(env, throw(env, OOM_ERROR_CLASS)))
+    })
+        .unwrap().unwrap();
+}
+
+#[test]
+fn check_error_on_exception_catch_javaexception_exact_class() {
+    EXECUTOR.with_attached(|env: &JNIEnv| {
+        Ok(check_error_on_exception(env, throw(env, EXCEPTION_CLASS))
+            .map_err(|e| assert!(e.starts_with("Java exception: java.lang.Exception")))
+            .expect_err("An exception should lead to an error"))
+    })
+        .unwrap();
+}
+
+#[test]
+fn check_error_on_exception_catch_javaexception_subclass() {
+    EXECUTOR.with_attached(|env: &JNIEnv| {
+        Ok(check_error_on_exception(env, throw(env, ARITHMETIC_EXCEPTION_CLASS))
+            .map_err(|e| assert!(e.starts_with("Java exception: java.lang.ArithmeticException")))
+            .expect_err("An exception should lead to an error"))
+    })
+        .unwrap();
+}
+
+#[test]
+#[should_panic(expected="JNI error: ")]
+fn check_error_on_exception_catch_jni_error() {
+    EXECUTOR.with_attached(|env: &JNIEnv| {
+        Ok(check_error_on_exception(env, make_jni_error()))
+    })
+        .unwrap().unwrap();
+}
+
+#[test]
+fn check_error_on_exception_dont_catch_good_result() {
+    EXECUTOR.with_attached(|env: &JNIEnv| {
+        Ok(check_error_on_exception(env, Ok(())))
+    })
+        .unwrap().unwrap();
+}
+
 fn throw(env: &JNIEnv, e: &str) -> JNIResult<()> {
     env.throw((e, ""))?;
     Err(ErrorKind::JavaException.into())
+}
+
+fn make_jni_error() -> JNIResult<()> {
+    Err(ErrorKind::Other(111).into())
 }
