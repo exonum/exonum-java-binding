42ea4b6e372788075f5a4e4c44c75ae515784d81
Dmitry Timofeev <dmitry-timofeev@users.noreply.github.com>
2018-04-06 12:33:46 +0300

    Update ServiceModule.java

--- exonum-java-binding-service-archetype/src/main/resources/archetype-resources/src/main/java/ServiceModule.java
+++ exonum-java-binding-service-archetype/src/main/resources/archetype-resources/src/main/java/ServiceModule.java
@@ -4,6 +4,9 @@ import com.exonum.binding.service.Service;
 import com.exonum.binding.service.TransactionConverter;
 import com.google.inject.AbstractModule;
 
+/**
+ * A service module defines bindings required to create an instance of {@link $.MyService}.
+ */
 public class ServiceModule extends AbstractModule {
 
   @Override
