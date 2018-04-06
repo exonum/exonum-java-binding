7a5918bcf6399b0e9d29d70cf9d80f1dd80748d1
Dmitry Timofeev <dmitry-timofeev@users.noreply.github.com>
2018-04-06 12:32:26 +0300

    Update MySchema Javadoc

--- exonum-java-binding-service-archetype/src/main/resources/archetype-resources/src/main/java/MySchema.java
+++ exonum-java-binding-service-archetype/src/main/resources/archetype-resources/src/main/java/MySchema.java
@@ -5,6 +5,12 @@ import com.exonum.binding.storage.database.View;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * {@code MySchema} provides access to the tables of {@link $.MyService},
+ * given a database state: a {@link View}.
+ *
+ * @see <a href="https://exonum.com/doc/architecture/storage/#table-types">Exonum table types.</a>
+ */
 public class MySchema implements Schema {
 
   private final View view;
