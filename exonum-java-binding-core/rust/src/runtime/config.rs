369a379629bbe707c7b8dbfe347c8546ebf92c09
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-04-18 11:06:39 +0300

    Document config mod

--- exonum-java-binding-core/rust/src/runtime/config.rs
+++ exonum-java-binding-core/rust/src/runtime/config.rs
@@ -1,14 +1,23 @@
+/// JavaServiceRuntime configuration.
 pub struct Config {
     pub jvm_config: JvmConfig,
     pub service_config: ServiceConfig,
 }
 
+/// JVM configuration.
 pub struct JvmConfig {
+    /// Whether to create JVM with `Xdebug` option or not.
     pub debug: bool,
+    /// Optional classpath.
     pub classpath: Option<String>,
 }
 
+/// Java service configuration.
 pub struct ServiceConfig {
+    /// Service module name.
+    ///
+    /// Must be subclass of `AbstractModule` and contain no-arguments constructor.
     pub module_name: String,
+    /// Port number for server.
     pub port: i32,
 }
