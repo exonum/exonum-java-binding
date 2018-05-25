6c5a7473b7f2f35c2f4c9fa947aa04c0e0b1182d
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2018-05-25 10:43:38 +0300

    Add more tests.

--- exonum-java-binding-core/src/test/java/com/exonum/binding/crypto/CryptoFunctionTest.java
+++ exonum-java-binding-core/src/test/java/com/exonum/binding/crypto/CryptoFunctionTest.java
@@ -23,6 +23,16 @@ public class CryptoFunctionTest {
   }
 
   @Test
+  public void validSignatureEmptyMessageVerificationTest() {
+    KeyPair keyPair = CRYPTO_FUNCTION.generateKeyPair();
+    PrivateKey privateKey = keyPair.getPrivateKey();
+    PublicKey publicKey = keyPair.getPublicKey();
+    byte[] emptyMessage = new byte[0];
+    byte[] signature = CRYPTO_FUNCTION.signMessage(emptyMessage, privateKey);
+    assertTrue(CRYPTO_FUNCTION.verify(emptyMessage, signature, publicKey));
+  }
+
+  @Test
   public void invalidLengthSignatureVerificationTest() {
     KeyPair keyPair = CRYPTO_FUNCTION.generateKeyPair();
     PublicKey publicKey = keyPair.getPublicKey();
@@ -38,6 +48,34 @@ public class CryptoFunctionTest {
     byte[] message = bytes("myMessage");
     byte[] invalidSignature = Bytes.createPrefixed(message, CRYPTO_SIGN_ED25519_BYTES);
     assertFalse(CRYPTO_FUNCTION.verify(message, invalidSignature, publicKey));
+  }
+
+  @Test
+  public void invalidPublicKeyVerificationTest() {
+    // Generate a key pair.
+    KeyPair keyPair = CRYPTO_FUNCTION.generateKeyPair();
+    PrivateKey privateKey = keyPair.getPrivateKey();
+    byte[] message = bytes("myMessage");
+    // Sign a message properly.
+    byte[] signature = CRYPTO_FUNCTION.signMessage(message, privateKey);
+
+    // Try to use another public key.
+    PublicKey publicKey = CRYPTO_FUNCTION.generateKeyPair().getPublicKey();
+    assertFalse(CRYPTO_FUNCTION.verify(message, signature, publicKey));
+  }
+
+  @Test
+  public void invalidPublicKeyLengthVerificationTest() {
+    // Generate a key pair.
+    KeyPair keyPair = CRYPTO_FUNCTION.generateKeyPair();
+    PrivateKey privateKey = keyPair.getPrivateKey();
+    byte[] message = bytes("myMessage");
+    // Sign a message.
+    byte[] signature = CRYPTO_FUNCTION.signMessage(message, privateKey);
+
+    // Try to use a public key of incorrect length.
+    PublicKey publicKey = PublicKey.fromHexString("abcd");
+    assertFalse(CRYPTO_FUNCTION.verify(message, signature, publicKey));
   }
 
   @Test
