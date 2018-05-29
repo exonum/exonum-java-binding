19b529cdbbe1f5f9f59238a2d446e8509c5dcf9c
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-05-29 14:47:52 +0300

    Remove unused imports in service_runtime tests

--- exonum-java-binding-core/rust/integration_tests/tests/service_runtime.rs
+++ exonum-java-binding-core/rust/integration_tests/tests/service_runtime.rs
@@ -1,15 +1,10 @@
 extern crate integration_tests;
 extern crate java_bindings;
-#[macro_use]
-extern crate lazy_static;
 extern crate exonum_testkit;
 
-use java_bindings::jni::JavaVM;
-use std::sync::Arc;
-use java_bindings::{DumbExecutor, ServiceConfig, JvmConfig, Config, JavaServiceRuntime};
-use java_bindings::exonum::helpers::fabric::NodeBuilder;
-use integration_tests::vm::{create_vm_for_tests_with_fake_classes, get_classpath, get_libpath};
-use exonum_testkit::{TestKit, TestKitBuilder};
+use java_bindings::{ServiceConfig, JvmConfig, Config, JavaServiceRuntime};
+use integration_tests::vm::{get_classpath, get_libpath};
+use exonum_testkit::TestKitBuilder;
 
 const TEST_SERVICE_MODULE_NAME: &str = "com.exonum.binding.fakes.services.service.TestServiceModule";
 
