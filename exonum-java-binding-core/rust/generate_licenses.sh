f8001de8da96cdb75c53b7317e1c68196b261569
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-06-14 09:16:54 +0300

    Generate native licenses

---
+++ exonum-java-binding-core/rust/generate_licenses.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Gather licenses for every Rust dependency and print it in the file in a readable form.
+
+cargo license -h || cargo install cargo-license
+
+cargo license -da | sed 's@registry+https://github.com/rust-lang/crates.io-index, @@g' | sed 's/\x1b\[[0-9;]*m//g' > NATIVE_LICENSES
+
