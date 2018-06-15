33376bf4100eb454c1409456d19daf09029479ee
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2018-06-15 20:51:07 +0300

    Use the Java home that Maven reports in the scripts setting LD_LIBRARY_PATH

--- run_native_integration_tests.sh
+++ run_native_integration_tests.sh
@@ -7,11 +7,11 @@
 # Fail immediately in case of errors and/or unset variables
 set -eu -o pipefail
 
-# Use an already set JAVA_HOME, or infer it from java.home system property.
+# Use the Java that Maven uses.
 #
 # Unfortunately, a simple `which java` will not work for some users (e.g., jenv),
 # hence this a bit complex thing.
-JAVA_HOME="${JAVA_HOME:-$(mvn --version | grep 'Java home' | sed 's/.*: //')}"
+JAVA_HOME="$(mvn --version | grep 'Java home' | sed 's/.*: //')"
 echo "JAVA_HOME=${JAVA_HOME}"
 
 # Find the directory containing libjvm (the relative path has changed in Java 9)
