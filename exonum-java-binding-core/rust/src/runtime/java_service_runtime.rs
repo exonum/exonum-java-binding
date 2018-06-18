8036e1ba7063b06e5f3e5e643902a76beaed684a
Alexander Irbis <irbis.labs@gmail.com>
2018-06-18 14:38:52 +0300

    Fix clippy issues.

--- exonum-java-binding-core/rust/src/runtime/java_service_runtime.rs
+++ exonum-java-binding-core/rust/src/runtime/java_service_runtime.rs
@@ -29,7 +29,7 @@ impl JavaServiceRuntime {
     ///
     /// There can be only one `JavaServiceRuntime` instance at a time.
     pub fn get_or_create(config: Config) -> Self {
-        let runtime = unsafe {
+        unsafe {
             // Initialize runtime if it wasn't created before.
             JAVA_SERVICE_RUNTIME_INIT.call_once(|| {
                 let java_vm = Self::create_java_vm(config.jvm_config);
@@ -45,8 +45,7 @@ impl JavaServiceRuntime {
             JAVA_SERVICE_RUNTIME.clone().expect(
                 "Trying to return runtime, but it's uninitialized",
             )
-        };
-        runtime
+        }
     }
 
     /// Returns internal service proxy.
