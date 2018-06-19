e9c44408c1b33f62e9c8ce1e81a0394d338afd32
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-06-19 11:24:26 +0300

    Modify runpath of libjava_bindings.so as well

--- exonum-java-binding-core/rust/ejb-app/modify_runpath.sh
+++ exonum-java-binding-core/rust/ejb-app/modify_runpath.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-# Modifies RUNPATH of ejb-app executable to make it able to locate java_binding.so and Rust std lib .so
+# Modifies RUNPATH of ejb-app executable to make it able to locate java_bindings.so and Rust std lib .so
 # EJB App executable must be in the current directory, dynamic libraries in the `lib` subdirectory.
 
 chrpath -v || { echo "No chrpath installed. Aborting"; exit 1; }
 
 # Convert RPATH to RUNPATH
 chrpath -c ejb-app
+chrpath -c lib/libjava_bindings.so
 
-# Modify RUNPATH
+# Modify RUNPATH of App
 chrpath ejb-app -r \$ORIGIN/lib
+
+# Modify RUNPATH of libjava_bindings
+chrpath lib/libjava_bindings.so -r \$ORIGIN
