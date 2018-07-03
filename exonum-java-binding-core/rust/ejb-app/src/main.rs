7a0327579adc5e47e4a3cddc7d85d5ce5589118d
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-07-04 00:09:01 +0300

    Add list of services to enable [ECR-1766]

--- exonum-java-binding-core/rust/ejb-app/src/main.rs
+++ exonum-java-binding-core/rust/ejb-app/src/main.rs
@@ -1,12 +1,30 @@
 extern crate exonum_configuration;
 extern crate java_bindings;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 
 use exonum_configuration::ServiceFactory as ConfigurationServiceFactory;
+use java_bindings::exonum::helpers::config::ConfigFile;
 use java_bindings::exonum::helpers::fabric;
 
+const PATH_TO_SERVICES_TO_ENABLE: &str = "ejb_app_services.toml";
+
+#[derive(Serialize, Deserialize)]
+struct ServicesToEnable {
+    services: Vec<String>,
+}
+
 fn main() {
-    let builder = fabric::NodeBuilder::new()
-        .with_service(Box::new(java_bindings::JavaServiceFactory))
-        .with_service(Box::new(ConfigurationServiceFactory));
+    let services: ServicesToEnable = ConfigFile::load(PATH_TO_SERVICES_TO_ENABLE).unwrap();
+    let mut builder =
+        fabric::NodeBuilder::new().with_service(Box::new(java_bindings::JavaServiceFactory));
+    for service in &services.services {
+        if service == "configuration" {
+            builder = builder.with_service(Box::new(ConfigurationServiceFactory));
+        } else {
+            panic!("Found unknown service name {}", service);
+        }
+    }
     builder.run()
 }
