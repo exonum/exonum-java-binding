ee5d38667f74cf32ee23b6d8ae2d038439510db7
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-07-04 10:51:56 +0300

    Fix script & newlines

--- exonum-java-binding-core/rust/ejb-app/start_cryptocurrency_node.sh
+++ exonum-java-binding-core/rust/ejb-app/start_cryptocurrency_node.sh
@@ -58,13 +58,13 @@ rm -rf testnet
 mkdir testnet
 
 header "GENERATE COMMON CONFIG"
-cargo run -- generate-template --validators-count=1 testnet/common.toml
+ejb-app generate-template --validators-count=1 testnet/common.toml
 
 header "GENERATE CONFIG"
-cargo run -- generate-config testnet/common.toml testnet/pub.toml testnet/sec.toml --ejb-classpath $EJB_CLASSPATH --ejb-libpath $EJB_LIBPATH --ejb-log-config-path $EJB_LOG_CONFIG_PATH --ejb-debug false --peer-address 127.0.0.1:5400
+ejb-app generate-config testnet/common.toml testnet/pub.toml testnet/sec.toml --ejb-classpath $EJB_CLASSPATH --ejb-libpath $EJB_LIBPATH --ejb-log-config-path $EJB_LOG_CONFIG_PATH --ejb-debug false --peer-address 127.0.0.1:5400
 
 header "FINALIZE"
-cargo run -- finalize testnet/sec.toml testnet/node.toml --ejb-module-name 'com.exonum.binding.cryptocurrency.ServiceModule' --ejb-port 6000 --public-configs testnet/pub.toml
+ejb-app finalize testnet/sec.toml testnet/node.toml --ejb-module-name 'com.exonum.binding.cryptocurrency.ServiceModule' --ejb-port 6000 --public-configs testnet/pub.toml
 
 header "START TESTNET"
-cargo run -- run -d testnet/db -c testnet/node.toml --public-api-address 127.0.0.1:3000
+ejb-app run -d testnet/db -c testnet/node.toml --public-api-address 127.0.0.1:3000
