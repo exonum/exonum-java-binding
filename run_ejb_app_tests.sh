5af6dcafbd35a59511ae593ff8affe0216bf7aeb
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-07-07 16:30:15 +0300

    Add list of services to enable [ECR-1766] & bundle btc-anchoring [ECR-1366] (#338):
    The list of service to enable is specified in a separate
    configuration file that must be in the current working directory.
    If no file is present, the configuration updater service and 
    the Java service are enabled.

---
+++ run_ejb_app_tests.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Runs EJB App tests (ejb-core/rust/ejb-app).
+#
+# ¡Keep it MacOS/Ubuntu compatible!
+
+# Fail immediately in case of errors and/or unset variables
+set -eu -o pipefail
+
+# Use the Java that Maven uses.
+#
+# Unfortunately, a simple `which java` will not work for some users (e.g., jenv),
+# hence this a bit complex thing.
+JAVA_HOME="$(mvn --version | grep 'Java home' | sed 's/.*: //')"
+echo "JAVA_HOME=${JAVA_HOME}"
+
+# Find the directory containing libjvm (the relative path has changed in Java 9)
+export LD_LIBRARY_PATH="$(find ${JAVA_HOME} -type f -name libjvm.* | xargs -n1 dirname)"
+echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
+
+cd exonum-java-binding-core/rust
+
+# Stable works well unless you want benchmarks.
+RUST_COMPILER_VERSION="stable"
+
+cargo "+${RUST_COMPILER_VERSION}" test \
+  --manifest-path ejb-app/Cargo.toml
