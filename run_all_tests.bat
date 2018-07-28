b7b6bc711ae269882f9de787dcf387671d4a0c81
Alexander Irbis <irbis.labs@gmail.com>
2018-07-28 21:21:25 +0300

    Update run script.

--- run_all_tests.bat
+++ run_all_tests.bat
@@ -1,23 +1,40 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_172
-rem @echo %JAVA_HOME%
-set JAVA_LIB_DIR=%JAVA_HOME%\jre\bin\server
-rem @echo %JAVA_LIB_DIR%
-set PATH=%PATH%;%JAVA_LIB_DIR%;%~dp0;%~dp0lib;C:\devel\pro\exonum-java-binding\exonum-java-binding-core\rust\target\debug
+@echo off
+
+if not defined JAVA_HOME (
+	for /f "tokens=2 delims==" %%a in ('java -XshowSettings:properties -version ^2^>^&^1 ^| findstr "java.home"') do set JAVA_HOME=%%a
+	set JAVA_HOME=%JAVA_HOME:~1%
+)
+echo JAVA_HOME=%JAVA_HOME%
+
+for /f "usebackq tokens=* delims=;" %%a in (`cmd /c dir /S "%JAVA_HOME%\jvm.dll" /s /b`) do set JAVA_LIB_DIR=%%a
+if "%JAVA_LIB_DIR%" == "" (
+    echo "jvm.dll not found. Perhaps you run JRE instead of JDK?"
+    exit 1
+) else (
+    set JAVA_LIB_DIR=%JAVA_LIB_DIR:~,-8%
+)
+echo JAVA_LIB_DIR=%JAVA_LIB_DIR%
+
+set PATH=%PATH%;%JAVA_LIB_DIR%
 set PATH=%PATH%;C:\Users\User\.rustup\toolchains\stable-x86_64-pc-windows-msvc\lib\rustlib\x86_64-pc-windows-msvc\lib\
-set PATH=%PATH%;C:\devel\pro\exonum-java-binding\lib
-rem @echo %PATH%
-
-rem Workaround for the issue https://github.com/briansmith/ring/issues/648
-set CL=/wd5045
-
-rem set SODIUM_LIB_DIR=C:\devel\pro\exonum-java-binding\lib
+set PATH=%PATH%;%~dp0exonum-java-binding-core\rust\target\debug
+set PATH=%PATH%;%~dp0lib
+set PATH=%PATH%;%~dp0
+echo PATH=%PATH%
 
 where jvm.dll
 
-set RUSTFLAGS=%RUSTFLAGS% -C prefer-dynamic
+rem Workaround for the issue https://github.com/briansmith/ring/issues/648
+set CL=/wd5045
+echo CL=%CL%
+
+rem set SODIUM_LIB_DIR=C:\devel\pro\exonum-java-binding\lib
+
+rem set RUSTFLAGS=%RUSTFLAGS% -C prefer-dynamic
 
 timeout 3
 
-rem mvn install -DskipTests --activate-profile`s ci-build -Drust.compiler.version="stable"
+rem mvn install -DskipTests --activate-profiles ci-build -Drust.compiler.version="stable"
 rem mvn install -DskipTests -Drust.compiler.version="stable"
+rem mvn clean install -Drust.compiler.version="stable" -Dtest=Ed25519CryptoFunctionTest -DfailIfNoTests=false
 mvn clean install -Drust.compiler.version="stable"
