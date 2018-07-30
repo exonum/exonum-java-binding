b81396d07d8a1d7e5d56e698e20c5654bb0bd7af
Alexander Irbis <irbis.labs@gmail.com>
2018-07-30 13:42:58 +0300

    Include paths to shared libraries in artefacts [ECR-1793]: (#336)
    Add paths to runtime libraries required to run the application or load 
    the library during image generation. This allows the linker to find
    the required shared libraries on a local machine without extra
    environment variables (= LD_LIBRARY_PATH on Linux/Mac),
    some of which cannot be easily passed with enabled SIP on Mac.
    
    The paths can still be overridden with LD_LIBRARY_PATH.
    
    See the docs in "tests_profile" for more details.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -7,19 +7,9 @@
 # Fail immediately in case of errors and/or unset variables
 set -eu -o pipefail
 
-# todo: All Java tests (Unit & ITs) are skipped until lib loading is fixed [https://jira.bf.local/browse/ECR-942].
-# Run unit and integration tests in ci-build profile. This profile includes:
-#  - Java unit & integration tests, including ci-only & slow non-critical tests,
-#    which are excluded in the default profile.
-#  - Checkstyle checks as errors.
-#  - Native unit & integration tests that do not require a JVM.
-# See build definitions of the modules for more.
-# TODO: stable does not work well until ECR-1839 is resolved
-mvn install \
-  -DskipTests \
-  --activate-profiles ci-build \
-  -Drust.compiler.version="${RUST_VERSION:-1.26.2}"
+# Run all java tests and native unit tests.
+./run_maven_tests.sh
 
-# Run native integration tests that require a JVM.
+# Run native integration tests that require prepared classpaths for fake classes.
 ./run_native_integration_tests.sh --skip-compile
-./run_ejb_app_tests.sh --skip-compile
+./run_ejb_app_tests.sh
