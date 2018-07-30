6c81448bc9bc3952a8261b31459a34461ec62550
Dmitry Timofeev <dmittimofeev@ya.ru>
2018-07-30 22:08:02 +0300

    Fix scripts running tests:
    The logic enabling tests has been moved to Maven build definitions,
    therefore, there is no need to set the global user property
    that will disable *all* tests (see 5e2055d).
    
    Also, when compiling artefacts in run_native_integration_tests.sh,
    there is no need to pass -DskipTests.

--- run_maven_tests.sh
+++ run_maven_tests.sh
@@ -7,7 +7,6 @@ set -eu -o pipefail
 # Import necessary environment variables (see the tests_profile header comment for details).
 source tests_profile
 
-# todo: All Java tests (Unit & ITs) are skipped until lib loading is fixed [https://jira.bf.local/browse/ECR-942].
 # Run unit and integration tests in ci-build profile. This profile includes:
 #  - Java unit & integration tests, including ci-only & slow non-critical tests,
 #    which are excluded in the default profile.
@@ -15,6 +14,5 @@ source tests_profile
 #  - Native unit & integration tests that do not require a JVM.
 # See build definitions of the modules for more.
 mvn install \
-  -DskipTests \
   --activate-profiles ci-build \
   -Drust.compiler.version="${RUST_COMPILER_VERSION}"
