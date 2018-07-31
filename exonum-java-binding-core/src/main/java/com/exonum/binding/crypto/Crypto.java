cd7dbadcc1869a896ee6a9c8cdcb575565aef244
Oleg Bondar <bullet-tooth@users.noreply.github.com>
2018-07-31 16:46:30 +0300

    fix build

--- exonum-java-binding-core/src/main/java/com/exonum/binding/crypto/Crypto.java
+++ exonum-java-binding-core/src/main/java/com/exonum/binding/crypto/Crypto.java
@@ -5,10 +5,10 @@ import com.goterl.lazycode.lazysodium.interfaces.Sign;
 public class Crypto {
 
   public static class Ed25519 {
-    public static int SEED_BYTES = Sign.ED25519_SEEDBYTES;
-    public static int SIGNATURE_BYTES = Sign.ED25519_BYTES;
-    public static int PRIVATE_KEY_BYTES = Sign.SECRETKEYBYTES;
-    public static int PUBLIC_KEY_BYTES = Sign.PUBLICKEYBYTES;
+    public static final int SEED_BYTES = Sign.ED25519_SEEDBYTES;
+    public static final int SIGNATURE_BYTES = Sign.ED25519_BYTES;
+    public static final int PRIVATE_KEY_BYTES = Sign.SECRETKEYBYTES;
+    public static final int PUBLIC_KEY_BYTES = Sign.PUBLICKEYBYTES;
   }
 
 }
