4c6f851854110637c8723c7d9ac56729f07f096c
Alexander Irbis <irbis.labs@gmail.com>
2018-07-31 14:04:23 +0300

    Add tests for `get_exception_message`. Make `get_exception_stack_trace` for internal development use only.

--- exonum-java-binding-core/rust/src/utils/jni.rs
+++ exonum-java-binding-core/rust/src/utils/jni.rs
@@ -30,25 +30,33 @@ pub fn get_exception_message(env: &JNIEnv, exception: JObject) -> JniResult<Opti
 ///
 /// `exception` should extend `java.lang.Throwable` and be not null
 pub fn get_exception_stack_trace(env: &JNIEnv, exception: JObject) -> JniResult<Option<String>> {
-    assert!(!exception.is_null(), "Invalid exception argument");
-    let frames = env.call_method(
-        exception,
-        "getStackTrace",
-        "()[Ljava/lang/StackTraceElement;",
-        &[],
-    )?
-        .l()?;
-    if frames.is_null() {
-        return Ok(None);
+    if cfg!(debug_assertions) {
+        assert!(!exception.is_null(), "Invalid exception argument");
+        let frames = env.call_method(
+            exception,
+            "getStackTrace",
+            "()[Ljava/lang/StackTraceElement;",
+            &[],
+        )?
+            .l()?;
+        if frames.is_null() {
+            return Ok(None);
+        }
+        let frames = frames.into_inner();
+        let frames_len = env.get_array_length(frames)?;
+        let mut stack: Vec<String> = Vec::with_capacity(frames_len as usize);
+        for i in 0..frames_len {
+            let frame = env.get_object_array_element(frames, i)?;
+            let frame_string = env.call_method(frame, "toString", "()Ljava/lang/String;", &[])?;
+            let frame_string = format!("    at {}\n", convert_to_string(env, frame_string.l()?)?);
+            stack.push(frame_string);
+        }
+        Ok(Some(stack.join("")))
+    } else {
+        // Stack traces are disabled in release builds.
+        //
+        // The responsibility for issuing debugging information in the production environment
+        // lies on the Java part of the framework.
+        Ok(None)
     }
-    let frames = frames.into_inner();
-    let frames_len = env.get_array_length(frames)?;
-    let mut stack: Vec<String> = Vec::with_capacity(frames_len as usize);
-    for i in 0..frames_len {
-        let frame = env.get_object_array_element(frames, i)?;
-        let frame_string = env.call_method(frame, "toString", "()Ljava/lang/String;", &[])?;
-        let frame_string = format!("    at {}\n", convert_to_string(env, frame_string.l()?)?);
-        stack.push(frame_string);
-    }
-    Ok(Some(stack.join("")))
 }
