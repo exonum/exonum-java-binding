0749579c83c1bbd1d84dd7a8000af5686d583dab
atrykush <andrii.trykush@gmail.com>
2018-08-03 15:50:33 +0300

    fixed path issue and compiler installation

--- run_rust_checks.sh
+++ run_rust_checks.sh
@@ -3,15 +3,15 @@
 # If CHECK_RUST is true, performs rust code checks,
 # otherwise skips checking rust code
 
-perform_rust_checks() {
-    echo 'Performing checks over the rust code'
-    # echo $CHECK_RUST
+# - RUST_COMPILER_VERSION=1.26.2
+# - RUST_NIGHTLY_VERSION=nightly-2018-06-29
+# - RUST_CLIPPY_VERSION=0.0.211
+# - EJB_RUST_BUILD_DIR="$TRAVIS_BUILD_DIR/exonum-java-binding-core/rust/"
+
+install_rust_compiler() {
     export PATH="$PATH":"$HOME/.cargo/bin"
     # Install rustup if it's not already installed (i.e., not in CI cache).
     which rustup > /dev/null || curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain "$RUST_COMPILER_VERSION"
-    # Install nightly rust version and clippy.
-    rustup toolchain install $RUST_NIGHTLY_VERSION
-    cargo +$RUST_NIGHTLY_VERSION clippy -V | grep $RUST_CLIPPY_VERSION || cargo +$RUST_NIGHTLY_VERSION install clippy --vers $RUST_CLIPPY_VERSION --force
     # Save the path to stdlib of the default toolchain to use it in scripts.
     export RUST_LIB_DIR=$(rustup run "$RUST_VERSION" rustc --print sysroot)/lib
     # Install rustfmt.
@@ -22,7 +22,7 @@ perform_rust_checks() {
     rustup run stable rustfmt -V
     rustup default "$RUST_COMPILER_VERSION"
     # Install cargo-audit if it's not already.
-    cargo-audit -V || cargo install cargo-audit --force
+    cargo-audit -V || cargo install cargo-audit --force # ~5 mins
     # List all installed cargo packages.
     cargo install --list
     # libjava_bindings.so and native application both need to load common Rust libs (eg libstd.so).
@@ -31,8 +31,21 @@ perform_rust_checks() {
     export SODIUM_BUILD=1
     export ROCKSDB_BUILD=1
     export SNAPPY_BUILD=1
+
+}
+
+install_rust_nightly() {
+    # Install nightly rust version and clippy.
+    rustup toolchain install $RUST_NIGHTLY_VERSION
+    cargo +$RUST_NIGHTLY_VERSION clippy -V | grep $RUST_CLIPPY_VERSION || cargo +$RUST_NIGHTLY_VERSION install clippy --vers $RUST_CLIPPY_VERSION --force
+
+}
+
+perform_rust_checks() {
+    echo 'Performing checks over the rust code'
+    # about 10 minutes to get to this step
     cd "${EJB_RUST_BUILD_DIR}"
-    cargo +stable fmt --all -- --write-mode=check
+    cargo +stable fmt --all -- --check
     # TODO Remove when clippy is fixed https://github.com/rust-lang-nursery/rust-clippy/issues/2831
     # Next 2 lines are a workaround to prevent clippy checking dependencies.
     cargo +${RUST_NIGHTLY_VERSION} check
@@ -48,8 +61,11 @@ skip_rust_checks() {
     echo 'Skipping checks'
 }
 
+install_rust_compiler
+
 if [ "$CHECK_RUST" = true ] 
 then
+    install_rust_nightly
     perform_rust_checks
 else
     skip_rust_checks
