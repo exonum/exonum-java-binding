f8d476975bcac207ef7e2f624a6a9f37c57b67a4
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2018-08-28 16:19:20 +0300

    Adaptation for working with Exonum version 0.9

--- exonum-java-binding-core/rust/src/runtime/java_service_runtime.rs
+++ exonum-java-binding-core/rust/src/runtime/java_service_runtime.rs
@@ -1,5 +1,5 @@
 use exonum::blockchain::Service;
-use exonum::helpers::fabric::{CommandExtension, Context, ServiceFactory};
+use exonum::helpers::fabric::{self, Command, CommandExtension, CommandName, Context, ServiceFactory};
 use jni::{self, JavaVM};
 
 use std::env;
@@ -117,12 +117,15 @@ pub fn panic_if_java_options() {
 pub struct JavaServiceFactory;
 
 impl ServiceFactory for JavaServiceFactory {
-    fn command(&mut self, command: &str) -> Option<Box<CommandExtension>> {
-        use exonum::helpers::fabric;
+    fn service_name(&self) -> &str {
+        "JAVA_SERVICE_FACTORY"
+    }
+
+    fn command(&mut self, command: CommandName) -> Option<Box<CommandExtension>> {
         // Execute EJB configuration steps along with standard Exonum Core steps.
         match command {
-            v if v == fabric::GenerateNodeConfig::name() => Some(Box::new(GenerateNodeConfig)),
-            v if v == fabric::Finalize::name() => Some(Box::new(Finalize)),
+            v if v == fabric::GenerateNodeConfig.name() => Some(Box::new(GenerateNodeConfig)),
+            v if v == fabric::Finalize.name() => Some(Box::new(Finalize)),
             _ => None,
         }
     }
