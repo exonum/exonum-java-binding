01b5f9a9c3280f8471ee494f69589616fa68f2e9
Sergei <sergei.makarov@bitfury.com>
2018-10-03 00:15:42 -0400

    ECR-1523 Review changes

--- run_all_tests_with_coverage.sh
+++ run_all_tests_with_coverage.sh
@@ -4,5 +4,5 @@
 # Run all java tests, native unit tests and native integration tests.
 ./run_all_tests.sh
 
-# Generate a coverage report.
-mvn org.jacoco:jacoco-maven-plugin:report org.eluder.coveralls:coveralls-maven-plugin:report
+# Update coveralls coverage report.
+mvn org.eluder.coveralls:coveralls-maven-plugin:report
