a0ade213ac2a1af00e0587421b20fbb93322fae3
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2018-10-03 13:55:07 +0300

    Remove run_all_tests_with_coverage
    It was misleading because run_all_tests.sh runs the tests *with*
    coverage (or, more precisely, ci-build profile of the Maven build).

--- run_all_tests_with_coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Run all tests, generate code coverage report and post it on coveralls.
-
-# Run all java tests, native unit tests and native integration tests.
-./run_all_tests.sh
-
-# Update coveralls coverage report.
-mvn org.eluder.coveralls:coveralls-maven-plugin:report
