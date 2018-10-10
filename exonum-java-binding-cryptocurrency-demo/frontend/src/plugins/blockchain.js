c7dde8d6241525c030eebfc2085c65147b4f5d53
SergNikolaev <iintersergey@gmail.com>
2018-10-10 13:49:19 +0300

    [ECR-2319] frontend updated to support binary transactions

--- exonum-java-binding-cryptocurrency-demo/frontend/src/plugins/blockchain.js
+++ exonum-java-binding-cryptocurrency-demo/frontend/src/plugins/blockchain.js
@@ -2,26 +2,25 @@ import * as Exonum from 'exonum-client'
 import axios from 'axios'
 import * as Protobuf from 'protobufjs/light'
 import bigInt from 'big-integer'
-import nacl from 'tweetnacl'
 
 const Root = Protobuf.Root
 const Type = Protobuf.Type
 const Field = Protobuf.Field
 
 //  schema for creating thansaction with protobuf
-let CreateTransactionProtobuf = new Type("CreateTransaction").add(new Field("ownerPublicKey", 1, "bytes"))
-CreateTransactionProtobuf.add(new Field("initialBalance", 2, "int64"))
+let CreateTransactionProtobuf = new Type('CreateTransaction').add(new Field('ownerPublicKey', 1, 'bytes'))
+CreateTransactionProtobuf.add(new Field('initialBalance', 2, 'int64'))
 
 // creating schema for transfer thansaction with protobuf
-let TransferTransactionProtobuf = new Type("TransferTransaction").add(new Field("seed", 1, "int64"))
-TransferTransactionProtobuf.add(new Field("senderId", 2, "bytes"))
-TransferTransactionProtobuf.add(new Field("recipientId", 3, "bytes"))
-TransferTransactionProtobuf.add(new Field("amount", 4, "int64"))
+let TransferTransactionProtobuf = new Type('TransferTransaction').add(new Field('seed', 1, 'int64'))
+TransferTransactionProtobuf.add(new Field('senderId', 2, 'bytes'))
+TransferTransactionProtobuf.add(new Field('recipientId', 3, 'bytes'))
+TransferTransactionProtobuf.add(new Field('amount', 4, 'int64'))
 
 // add types to protobuf root
-var root = new Root();
-root.define("CreateTransactionProtobuf").add(CreateTransactionProtobuf);
-root.define("TransferTransactionProtobuf").add(TransferTransactionProtobuf);
+var root = new Root()
+root.define('CreateTransactionProtobuf').add(CreateTransactionProtobuf)
+root.define('TransferTransactionProtobuf').add(TransferTransactionProtobuf)
 
 const TX_URL = '/api/cryptocurrency-demo-service/submit-transaction'
 const ATTEMPTS = 10
@@ -45,7 +44,7 @@ const MessageHead = Exonum.newType({
   ]
 })
 
-function getWallet(publicKey) {
+function getWallet (publicKey) {
   return axios.get(`/api/cryptocurrency-demo-service/wallet/${publicKey}`)
     .then(response => response.data)
     .then(data => {
@@ -56,12 +55,12 @@ function getWallet(publicKey) {
     })
 }
 
-function waitForAcceptance(publicKey, hash) {
+function waitForAcceptance (publicKey, hash) {
   let attempt = ATTEMPTS
 
-  return (function makeAttempt() {
+  return (function makeAttempt () {
     // find transaction in a explorer
-    return axios.get(`/api/explorer/v1/transactions/${hash}`).then(response =>  {
+    return axios.get(`/api/explorer/v1/transactions/${hash}`).then(response => {
       if (response.data.type !== 'committed') {
         if (--attempt > 0) {
           return new Promise(resolve => {
@@ -77,7 +76,7 @@ function waitForAcceptance(publicKey, hash) {
   })()
 }
 
-function createHeader(messageId) {
+function createHeader (messageId) {
   return MessageHead.serialize({
     network_id: 0,
     protocol_version: PROTOCOL_VERSION,
@@ -87,103 +86,90 @@ function createHeader(messageId) {
   })
 }
 
+const appendBuffer = function (buffer1, buffer2) {
+  const tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength)
+  tmp.set(new Uint8Array(buffer1), 0)
+  tmp.set(new Uint8Array(buffer2), buffer1.byteLength)
+  return tmp
+}
+
 module.exports = {
-  install(Vue) {
+  install (Vue) {
     Vue.prototype.$blockchain = {
-      generateKeyPair() {
+      generateKeyPair () {
         return Exonum.keyPair()
       },
 
-      generateSeed() {
+      generateSeed () {
         return bigInt.randBetween(0, MAX_VALUE).valueOf()
-      },  
+      },
 
-      createWallet(keyPair, balance) {
-
+      createWallet (keyPair, balance) {
         // serialize and append message header
         let buffer = createHeader(TX_WALLET_ID)
-
         let data = {
           ownerPublicKey: Exonum.hexadecimalToUint8Array(keyPair.publicKey),
           initialBalance: balance
         }
 
         // serialize and append message body
-        const body = CreateTransactionProtobuf.encode(data).finish();
-
-        body.forEach(element => {
-          buffer.push(element)
-        });
+        const body = CreateTransactionProtobuf.encode(data).finish()
+        const bufferWithBody = appendBuffer(new Uint8Array(buffer), body)
 
         // calculate payload and insert it into buffer
-        Exonum.Uint32.serialize(buffer.length + SIGNATURE_LENGTH, buffer, PAYLOD_SIZE_OFFSET)
+        Exonum.Uint32.serialize(bufferWithBody.length + SIGNATURE_LENGTH, bufferWithBody, PAYLOD_SIZE_OFFSET)
 
         // append signature
-        const signature = Exonum.sign(keyPair.secretKey, buffer)
+        const signature = Exonum.sign(keyPair.secretKey, bufferWithBody)
+        const signBuffer = appendBuffer(bufferWithBody, Exonum.hexadecimalToUint8Array(signature))
 
-        data.ownerPublicKey = keyPair.publicKey
-
-        return axios.post(TX_URL, {
-         protocol_version: PROTOCOL_VERSION,
-         service_id: SERVICE_ID,
-         message_id: TX_WALLET_ID,
-         signature: signature,
-         body: data
-        })
+        return axios.post(TX_URL, signBuffer)
       },
 
-      transfer(keyPair, receiver, amountToTransfer, seed) {
-        
+      transfer (keyPair, receiver, amountToTransfer, seed) {
         // serialize and append message header
         let buffer = createHeader(TX_TRANSFER_ID)
 
         const data = {
           seed: seed,
           senderId: Exonum.hexadecimalToUint8Array(keyPair.publicKey),
-          recipientId:  Exonum.hexadecimalToUint8Array(receiver),
+          recipientId: Exonum.hexadecimalToUint8Array(receiver),
           amount: amountToTransfer
         }
 
-        const body = TransferTransactionProtobuf.encode(data).finish();
-
-        body.forEach(element => {
-          buffer.push(element)
-        });
+        // serialize and append message body
+        const body = TransferTransactionProtobuf.encode(data).finish()
+        const bufferWithBody = appendBuffer(new Uint8Array(buffer), body)
 
         // calculate payload and insert it into buffer
-        Exonum.Uint32.serialize(buffer.length + SIGNATURE_LENGTH, buffer, PAYLOD_SIZE_OFFSET)
+        Exonum.Uint32.serialize(bufferWithBody.length + SIGNATURE_LENGTH, bufferWithBody, PAYLOD_SIZE_OFFSET)
 
         // append signature
-        const signature = Exonum.sign(keyPair.secretKey, buffer)
+        const signature = Exonum.sign(keyPair.secretKey, bufferWithBody)
+        const signBuffer = appendBuffer(bufferWithBody, Exonum.hexadecimalToUint8Array(signature))
 
-        data.senderId = keyPair.publicKey
-        data.recipientId = receiver
-        return axios.post(TX_URL, {
-          protocol_version: PROTOCOL_VERSION,
-          service_id: SERVICE_ID,
-          message_id: TX_TRANSFER_ID,
-          signature: signature,
-          body: data
-        }).then(response => waitForAcceptance(keyPair.publicKey, response.data))
+        return axios.post(TX_URL, signBuffer)
+          .then(response => waitForAcceptance(keyPair.publicKey, response.data))
       },
 
       getWallet: getWallet,
 
-      getBlocks(latest) {
+      getBlocks (latest) {
         const suffix = !isNaN(latest) ? '&latest=' + latest : ''
         return axios.get(`/api/explorer/v1/blocks?count=${PER_PAGE}${suffix}`).then(response => response.data)
       },
 
-      getBlock(height) {
+      getBlock (height) {
         return axios.get(`/api/explorer/v1/blocks/${height}`).then(response => response.data)
       },
 
-      getTransaction(hash) {
+      getTransaction (hash) {
         return axios.get(`/api/explorer/v1/transactions/${hash}`).then(response => {
-          return response.data })
+          return response.data
+        })
       },
 
-      getHistory (publicKey){
+      getHistory (publicKey) {
         return axios.get(`/api/cryptocurrency-demo-service/wallet/${publicKey}/history`).then(r => r.data)
       }
     }
