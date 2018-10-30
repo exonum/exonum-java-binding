8d889b56c58223a1cfdc8a6c55654ca42086fbf8
Oleg Bondar <bullet-tooth@users.noreply.github.com>
2018-10-30 15:12:35 +0200

    fmt

--- exonum-java-binding-core/src/test/java/com/exonum/binding/blockchain/BlockchainTest.java
+++ exonum-java-binding-core/src/test/java/com/exonum/binding/blockchain/BlockchainTest.java
@@ -17,7 +17,6 @@
 
 package com.exonum.binding.blockchain;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
