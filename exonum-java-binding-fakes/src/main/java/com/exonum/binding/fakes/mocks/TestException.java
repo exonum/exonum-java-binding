7bd11e39b0b268a9b795172f488b216fcfc649ac
Sergey Kletsun <sergey.kletsun@gmail.com>
2018-11-16 13:47:43 +0200

    Removed useless constructors from the TestException.

--- exonum-java-binding-fakes/src/main/java/com/exonum/binding/fakes/mocks/TestException.java
+++ exonum-java-binding-fakes/src/main/java/com/exonum/binding/fakes/mocks/TestException.java
@@ -1,22 +1,7 @@
 package com.exonum.binding.fakes.mocks;
 
 /**
- * Exception that's used for testing purposes only
+ * Marker exception that is used for testing purposes only
  */
 public class TestException extends RuntimeException {
-
-  /**
-   * Simple constructor to just detect an exception of a specific type
-   */
-  public TestException() {
-    super();
-  }
-
-  /**
-   * Constructor with message to detect specific context
-   * @param context
-   */
-  public TestException(String context) {
-    super(context);
-  }
 }
