46cb29b1c576d01e301a222df4b1dd1d17b366b4
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2018-11-28 18:20:03 +0200

    Require application/octet-stream content type for txs [ECR-2650]
    Require application/octet-stream content type for submitting
    transactions. application/x-www-form-urlencoded does not work
    for unescaped binary data.

--- exonum-java-binding-cryptocurrency-demo/src/main/java/com/exonum/binding/cryptocurrency/ApiController.java
+++ exonum-java-binding-cryptocurrency-demo/src/main/java/com/exonum/binding/cryptocurrency/ApiController.java
@@ -83,6 +83,9 @@ final class ApiController {
   }
 
   private void submitTransaction(RoutingContext rc) {
+    checkRequestContentType(rc, "application/octet-stream");
+
+    // Read the transaction message
     Buffer buffer = rc.getBody();
     BinaryMessage message = BinaryMessage.fromBytes(buffer.getBytes());
 
@@ -95,6 +98,14 @@ final class ApiController {
     rc.response()
         .putHeader("Content-Type", "text/plain")
         .end(String.valueOf(txHash));
+  }
+
+  private void checkRequestContentType(RoutingContext rc,
+      String expectedContentType) {
+    String contentType = rc.request().getHeader("Content-Type");
+
+    checkArgument(expectedContentType.equals(contentType), "Content-Type must be %s, but %s",
+        expectedContentType, contentType);
   }
 
   private void getWallet(RoutingContext rc) {
