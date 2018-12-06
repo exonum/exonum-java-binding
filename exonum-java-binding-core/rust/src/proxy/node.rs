20f93f8da0c3237208cb518c92e8227da9cba9f9
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-12-06 12:28:17 +0300

    Update to the latest Exonum version

--- exonum-java-binding-core/rust/src/proxy/node.rs
+++ exonum-java-binding-core/rust/src/proxy/node.rs
@@ -1,6 +1,5 @@
 use exonum::blockchain::Blockchain;
 use exonum::crypto::PublicKey;
-use exonum::messages::Message;
 use exonum::messages::RawTransaction;
 use exonum::messages::ServiceTransaction;
 use exonum::node::ApiSender;
@@ -65,16 +64,7 @@ impl NodeContext {
 
     #[doc(hidden)]
     pub fn submit(&self, transaction: RawTransaction) -> Result<(), failure::Error> {
-        let service_id = transaction.service_id();
-        // FIXME: using hidden service_keypair
-        let signed_transaction = Message::sign_transaction(
-            transaction.service_transaction(),
-            service_id,
-            self.public_key,
-            &self.blockchain.service_keypair.1,
-        );
-        self.transaction_sender
-            .broadcast_transaction(signed_transaction)
+        self.blockchain.broadcast_raw_transaction(transaction)
     }
 }
 
