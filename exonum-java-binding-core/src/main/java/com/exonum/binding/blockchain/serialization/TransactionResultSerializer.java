2f7561b59c309ee42de9f50b0b9de05ec5985ffe
Sergei <sergei.makarov@bitfury.com>
2018-12-16 22:50:51 -0500

    ECR-2712 Checkstyle fix

--- exonum-java-binding-core/src/main/java/com/exonum/binding/blockchain/serialization/TransactionResultSerializer.java
+++ exonum-java-binding-core/src/main/java/com/exonum/binding/blockchain/serialization/TransactionResultSerializer.java
@@ -54,7 +54,8 @@ public enum TransactionResultSerializer implements Serializer<TransactionResult>
     } else if (status == UNEXPECTED_ERROR_STATUS_CODE) {
       return TransactionResult.unexpectedError(description);
     } else {
-      String message = String.format("Invalid status code: %s, must be in the range [0, 257]", status);
+      String message =
+          String.format("Invalid status code: %s, must be in the range [0, 257]", status);
       throw new IllegalArgumentException(message);
     }
   }
