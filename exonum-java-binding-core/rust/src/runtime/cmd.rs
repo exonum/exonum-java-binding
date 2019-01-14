a20c4f040f3f76eaf69efe3ec731c0ffea9738c7
Sergey Kletsun <sergey.kletsun@gmail.com>
2019-01-14 10:27:57 +0200

    Additional comments, refactoring.

--- exonum-java-binding-core/rust/src/runtime/cmd.rs
+++ exonum-java-binding-core/rust/src/runtime/cmd.rs
@@ -5,8 +5,15 @@ use exonum::helpers::fabric::CommandExtension;
 use exonum::helpers::fabric::Context;
 use exonum::node::NodeConfig;
 use failure;
+use serde::Deserialize;
 use std::collections::BTreeMap;
 use toml::Value;
+
+/// This code encapsulates the logic of processing of our extensions to the node's binary command
+/// line arguments. The general idea: we have extensions to three regular commands of the node -
+/// `generate-config`, `finalize` and `run`. We process them on every step and store intermediate
+/// results to the persistent storage available on the specific step. Finally, after the `run` step
+/// we compose the `Config` structure that contains all required info for service initialization.
 
 const EJB_LOG_CONFIG_PATH: &str = "EJB_LOG_CONFIG_PATH";
 const EJB_CLASSPATH: &str = "EJB_CLASSPATH";
@@ -20,6 +27,9 @@ pub const EJB_CONFIG_SECTION_NAME: &str = "ejb";
 const EJB_CONFIG_KEY: &str = "ejb_config";
 const SERVICE_CONFIG_KEY: &str = "service_config";
 
+/// Encapsulates processing of extensions of the `generate-config` command. At this step we gather
+/// required parameters for EJB configuration and store them under the section for the secret
+/// configuration of services that becomes available at this step.
 pub struct GenerateNodeConfig;
 
 impl CommandExtension for GenerateNodeConfig {
@@ -53,6 +63,7 @@ impl CommandExtension for GenerateNodeConfig {
     }
 
     fn execute(&self, mut context: Context) -> Result<Context, failure::Error> {
+        // Get the arguments for current step
         let log_config_path = context.arg(EJB_LOG_CONFIG_PATH).unwrap_or_default();
         let class_path = context.arg(EJB_CLASSPATH)?;
         let lib_path = context.arg(EJB_LIBPATH)?;
@@ -63,25 +74,18 @@ impl CommandExtension for GenerateNodeConfig {
             log_config_path,
         };
 
-        // Keep `EjbConfig` under the `keys::SERVICES_SECRET_CONFIGS` section. Will be retrieved
-        // later at the `Finalize` step for further processing.
-        let mut services_secret_configs = context
-            .get(keys::SERVICES_SECRET_CONFIGS)
-            .unwrap_or_default();
-        services_secret_configs.extend(
-            vec![(
-                EJB_CONFIG_KEY.to_owned(),
-                Value::try_from(ejb_config).unwrap(),
-            )]
-            .into_iter(),
-        );
-
-        context.set(keys::SERVICES_SECRET_CONFIGS, services_secret_configs);
+        // Store `EjbConfig` under the secret services configs section. Will be retrieved later at
+        // the `Finalize` step for further processing.
+        update_secret_services_config(&mut context, Value::try_from(ejb_config)?);
 
         Ok(context)
     }
 }
 
+/// Encapsulates processing of extensions of the `finalize` command. At this step we gather
+/// required parameters for service configuration. Also, at this step the node config finalization
+/// happens, so we store there our newly created service configuration as well as the EJB
+/// configuration created at `generate-config` step.
 pub struct Finalize;
 
 impl CommandExtension for Finalize {
@@ -107,17 +111,16 @@ impl CommandExtension for Finalize {
     }
 
     fn execute(&self, mut context: Context) -> Result<Context, failure::Error> {
+        // Get the arguments for current step
         let module_name = context.arg(EJB_MODULE_NAME)?;
         let port = context.arg(EJB_PORT)?;
 
         let service_config = ServiceConfig { module_name, port };
 
         // Retrieve `EjbConfig` saved at the `GenerateNodeConfig` step
-        let ejb_config: EjbConfig = context
-            .get(keys::SERVICES_SECRET_CONFIGS)
-            .expect("Can't get services secret configs")
+        let ejb_config: EjbConfig = get_services_secret_configs(&context)
             .get(EJB_CONFIG_KEY)
-            .expect("Can't get JVM config")
+            .expect("Can't get EJB config")
             .clone()
             .try_into()?;
 
@@ -130,15 +133,16 @@ impl CommandExtension for Finalize {
             Value::try_from(service_config)?,
         );
 
-        let mut node_config: NodeConfig = context.get(keys::NODE_CONFIG)?;
-        node_config
-            .services_configs
-            .insert(EJB_CONFIG_SECTION_NAME.to_owned(), Value::try_from(config)?);
-        context.set(keys::NODE_CONFIG, node_config);
+        // Put config to NodeConfig and store updated NodeConfig back to Context
+        update_node_config(&mut context, Value::try_from(config)?);
+
         Ok(context)
     }
 }
 
+/// Encapsulates processing of extensions of the `run` command. At this step we gather optional
+/// parameters for JVM configuration and produce the complete EJB configuration that gets stored to
+/// the `Context` for further processing during the service initialization.
 pub struct Run;
 
 impl CommandExtension for Run {
@@ -175,6 +179,7 @@ impl CommandExtension for Run {
     }
 
     fn execute(&self, mut context: Context) -> Result<Context, failure::Error> {
+        // Get the arguments for current step
         let args_prepend: Vec<String> = context.arg_multiple(JVM_ARGS_PREPEND).unwrap_or_default();
         let args_append: Vec<String> = context.arg_multiple(JVM_ARGS_APPEND).unwrap_or_default();
         let jvm_debug_socket = context.arg(JVM_DEBUG_SOCKET).ok();
@@ -186,28 +191,10 @@ impl CommandExtension for Run {
             jvm_debug_socket,
         };
 
-        // Get a reference to the parts of configuration stored at previous step
-        let svc_config: BTreeMap<String, Value> = context
-            .get(keys::NODE_CONFIG)
-            .expect("Unable to read node configuration.")
-            .services_configs
-            .get(EJB_CONFIG_SECTION_NAME)
-            .expect("Unable to read EJB configuration.")
-            .clone()
-            .try_into()?;
-
-        // Retrieve `EjbConfig` and `ServiceConfig` saved at the `Finalize` step
-        let ejb_config = svc_config
-            .get(EJB_CONFIG_KEY)
-            .expect("Unable to read EjbConfig from node configuration")
-            .clone()
-            .try_into()?;
-
-        let service_config = svc_config
-            .get(SERVICE_CONFIG_KEY)
-            .expect("Unable to read ServiceConfig from node configuration")
-            .clone()
-            .try_into()?;
+        // Retrieve EjbConfig & ServiceConfig stored at previous step
+        let ejb_config: EjbConfig = extract_from_services_config(&context, EJB_CONFIG_KEY)?;
+        let service_config: ServiceConfig =
+            extract_from_services_config(&context, SERVICE_CONFIG_KEY)?;
 
         // Construct the complete EJB configuration
         let config = Config {
@@ -217,12 +204,65 @@ impl CommandExtension for Run {
         };
 
         // Store configuration to the context. It will be used during service's bootstrap.
-        let mut node_config: NodeConfig = context.get(keys::NODE_CONFIG)?;
-        node_config
-            .services_configs
-            .insert(EJB_CONFIG_SECTION_NAME.to_owned(), Value::try_from(config)?);
-        context.set(keys::NODE_CONFIG, node_config);
+        update_node_config(&mut context, Value::try_from(config)?);
 
         Ok(context)
     }
+}
+
+/// Updates the `ejb` section of services secret configs with `value` and stores it to the `Context`
+fn update_secret_services_config(context: &mut Context, value: Value) {
+    let mut services_secret_configs = get_services_secret_configs(&context);
+    services_secret_configs.extend(vec![(EJB_CONFIG_KEY.to_owned(), value)].into_iter());
+    context.set(keys::SERVICES_SECRET_CONFIGS, services_secret_configs);
+}
+
+/// Returns underlying map of services configs section
+fn get_services_secret_configs(context: &Context) -> BTreeMap<String, Value> {
+    context
+        .get(keys::SERVICES_SECRET_CONFIGS)
+        .expect("Can't get services secret configs")
+}
+
+/// Updates the `ejb` section of service configs of `NodeConfig` with `value` and puts updated
+/// `NodeConfig` back to `Context`
+fn update_node_config(context: &mut Context, value: Value) {
+    let mut node_config = get_node_config(context);
+    node_config
+        .services_configs
+        .insert(EJB_CONFIG_SECTION_NAME.to_owned(), value);
+    context.set(keys::NODE_CONFIG, node_config);
+}
+
+/// Extracts the `NodeConfig` from `Context` for further processing
+fn get_node_config(context: &Context) -> NodeConfig {
+    context
+        .get(keys::NODE_CONFIG)
+        .expect("Unable to read node configuration.")
+}
+
+/// Extracts entity from the service configs section of the `NodeConfig` in the `Context`
+fn extract_from_services_config<'de, T>(context: &Context, key: &str) -> Result<T, failure::Error>
+where
+    T: Deserialize<'de>,
+{
+    //  Get a reference to the EJB configuration
+    let service_config: BTreeMap<String, Value> = get_node_config(context)
+        .services_configs
+        .get(EJB_CONFIG_SECTION_NAME)
+        .expect("Unable to read EJB configuration.")
+        .clone()
+        .try_into()?;
+
+    // Retrieve config by key
+    let config = service_config
+        .get(key)
+        .expect(&format!(
+            "Unable to read config with key [{}] from node configuration",
+            key
+        ))
+        .clone()
+        .try_into()?;
+
+    Ok(config)
 }
