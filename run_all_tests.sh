25f6cbe54fd9e4cda91b7786974e52d9888a5923
Oleg Bondar <bullet-tooth@users.noreply.github.com>
2019-02-05 11:16:03 +0200

    restored comment

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Runs all tests.
 #
+# A JVM will be selected by JAVA_HOME environment variable, or, if it is not set,
+# inferred from the java executable available on the path.
 
 # Fail immediately in case of errors and/or unset variables
 set -eu -o pipefail
