889391c40041f068b358cc40b71461ce105dcb91
Oleg Bondar <bullet-tooth@users.noreply.github.com>
2019-02-12 09:56:55 +0200

    pr comments fix

--- exonum-light-client/src/main/java/com/exonum/client/ExonumClientImpl.java
+++ exonum-light-client/src/main/java/com/exonum/client/ExonumClientImpl.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2019 The Exonum Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.exonum.client;
 
 import static com.exonum.binding.common.serialization.json.JsonSerializer.json;
@@ -55,7 +72,7 @@ class ExonumClientImpl implements ExonumClient {
 
   private <T> T blockingExecuteWithResponse(Request request, Class<T> responseObject) {
     try (Response response = httpClient.newCall(request).execute()) {
-      if(!response.isSuccessful()) {
+      if (!response.isSuccessful()) {
         throw new RuntimeException("Execution wasn't success: " + response.toString());
       }
       String responseJson = response.body().string();
