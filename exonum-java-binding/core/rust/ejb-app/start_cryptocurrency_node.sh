d93db96d8f5a2ef05479d1587f8d4db98705530b
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-03-01 10:46:01 +0300

    Use maven configuration to run EJB App from target directory
    Also start_cryptocurrency_node.sh script was updated

--- exonum-java-binding/core/rust/ejb-app/start_cryptocurrency_node.sh
+++ exonum-java-binding/core/rust/ejb-app/start_cryptocurrency_node.sh
@@ -41,30 +41,35 @@ CRYPTOCURRENCY_TXT="cryptocurrency-demo/target/cryptocurrency-classpath.txt"
 EJB_CLASSPATH="$(cat ${EJB_ROOT}/${CRYPTOCURRENCY_TXT})"
 EJB_CLASSPATH="${EJB_CLASSPATH}:${EJB_ROOT}/cryptocurrency-demo/target/classes"
 echo "EJB_CLASSPATH=${EJB_CLASSPATH}"
-EJB_LOG_CONFIG_PATH="${EJB_APP_DIR}/log4j.xml"
+EJB_LOG_CONFIG_PATH="${EJB_APP_DIR}/log4j-fallback.xml"
 
-export LD_LIBRARY_PATH="$JVM_LIB_PATH"
+RUST_LIB_PATH="$(rustup run ${RUST_COMPILER_VERSION:-stable} rustc --print sysroot)/lib"
+echo "RUST_LIB_DIR: ${RUST_LIB_PATH}"
+
+export LD_LIBRARY_PATH="$JVM_LIB_PATH:$RUST_LIB_PATH"
 echo "Final LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
 
 # Clear test dir
 rm -rf testnet
 mkdir testnet
 
+ejb_app="${EJB_ROOT}/core/rust/target/debug/ejb-app"
+
 header "GENERATE COMMON CONFIG"
-cargo run -- generate-template --validators-count=1 testnet/common.toml \
+"${ejb_app}" generate-template --validators-count=1 testnet/common.toml \
  --ejb-module-name 'com.exonum.binding.cryptocurrency.ServiceModule'
 
 header "GENERATE CONFIG"
-cargo run -- generate-config testnet/common.toml testnet/pub.toml testnet/sec.toml \
+"${ejb_app}" generate-config testnet/common.toml testnet/pub.toml testnet/sec.toml \
  --peer-address 127.0.0.1:5400
 
 header "FINALIZE"
-cargo run -- finalize testnet/sec.toml testnet/node.toml \
+"${ejb_app}" finalize testnet/sec.toml testnet/node.toml \
  --ejb-service-classpath $EJB_CLASSPATH \
  --public-configs testnet/pub.toml
 
 header "START TESTNET"
-cargo run -- run -d testnet/db -c testnet/node.toml \
+"${ejb_app}" run -d testnet/db -c testnet/node.toml \
  --public-api-address 127.0.0.1:3000 \
  --ejb-log-config-path $EJB_LOG_CONFIG_PATH \
  --ejb-port 6000
