dcc876b0fe1a6bb6ba6a77a08c122ed840ed855b
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-03-11 18:14:12 +0300

    WIP

--- exonum-java-binding/core/rust/ejb-app/start_cryptocurrency_node.sh
+++ exonum-java-binding/core/rust/ejb-app/start_cryptocurrency_node.sh
@@ -46,7 +46,7 @@ EJB_LOG_CONFIG_PATH="${EJB_APP_DIR}/log4j-fallback.xml"
 RUST_LIB_PATH="$(rustup run ${RUST_COMPILER_VERSION:-1.32.0} rustc --print sysroot)/lib"
 echo "RUST_LIB_DIR: ${RUST_LIB_PATH}"
 
-export LD_LIBRARY_PATH="${EJB_ROOT}/core/rust/target/debug:$JVM_LIB_PATH:$RUST_LIB_PATH"
+export LD_LIBRARY_PATH="$JVM_LIB_PATH:$RUST_LIB_PATH"
 echo "Final LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
 
 # Clear test dir
