131553406f8d07603139dc973e218d8c3b1598a7
Dmitry Timofeev <dmitry-timofeev@users.noreply.github.com>
2019-03-15 18:42:17 +0200

    Add a ServiceArtifactBuilder smoke integration test [ECR-3014] (#783)
    Test that the artifact produced by the ServiceArtifactBuilder can
    be properly loaded by PF4J.

---
+++ exonum-java-binding/testing/src/test/java/com/exonum/binding/test/runtime/ServiceArtifactBuilderSmokeIntegrationTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2019 The Exonum Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.exonum.binding.test.runtime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.exonum.binding.test.runtime.testplugin.TestPlugin;
+import com.exonum.binding.test.runtime.testplugin.TestServiceExtension;
+import com.exonum.binding.test.runtime.testplugin.TestServiceExtensionImpl;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.pf4j.DefaultPluginManager;
+import org.pf4j.PluginManager;
+import org.pf4j.PluginState;
+import org.pf4j.PluginWrapper;
+
+class ServiceArtifactBuilderSmokeIntegrationTest {
+
+  @Test
+  @DisplayName("Created plugin must be successfully loaded and unloaded by the PluginManager. "
+      + "If this test does not work, subsequent use of ServiceArtifactBuilder in other ITs makes "
+      + "no sense.")
+  void createdArtifactCanBeLoaded(@TempDir Path tmp) throws IOException {
+    Path pluginPath = tmp.resolve("test-plugin.jar");
+
+    String pluginId = "test-plugin";
+    String version = "1.0.1";
+    new ServiceArtifactBuilder()
+        .setPluginId(pluginId)
+        .setPluginVersion(version)
+        .setManifestEntry("Plugin-Class", TestPlugin.class.getName())
+        .addClasses(
+            TestPlugin.class,
+            TestServiceExtensionImpl.class
+        )
+        .addExtensionEntry(TestServiceExtensionImpl.class.getName())
+        .writeTo(pluginPath);
+
+    PluginManager pluginManager = new DefaultPluginManager();
+
+    // Try to load the plugin
+    String loadedPluginId = pluginManager.loadPlugin(pluginPath);
+    assertThat(loadedPluginId).isEqualTo(pluginId);
+
+    // Check it has correct version
+    PluginWrapper plugin = pluginManager.getPlugin(pluginId);
+    assertThat(plugin.getDescriptor().getVersion()).isEqualTo(version);
+    assertNamesEqual(plugin.getPlugin().getClass(), TestPlugin.class);
+
+    // Try to start
+    PluginState pluginState = pluginManager.startPlugin(pluginId);
+    assertThat(pluginState).isEqualTo(PluginState.STARTED);
+
+    // Check the extensions
+    List<Class<TestServiceExtension>> extensionClasses = pluginManager
+        .getExtensionClasses(TestServiceExtension.class, pluginId);
+    assertThat(extensionClasses).hasSize(1);
+    Class<?> extensionType = extensionClasses.get(0);
+    assertNamesEqual(extensionType, TestServiceExtensionImpl.class);
+
+
+    // Try to stop and unload
+    pluginState = pluginManager.stopPlugin(pluginId);
+    assertThat(pluginState).isEqualTo(PluginState.STOPPED);
+    boolean unloadResult = pluginManager.unloadPlugin(pluginId);
+    assertTrue(unloadResult);
+  }
+
+  private static void assertNamesEqual(Class<?> actual, Class<?> expected) {
+    assertThat(actual.getName()).isEqualTo(expected.getName());
+  }
+}
--- exonum-java-binding/testing/src/test/java/com/exonum/binding/test/runtime/ServiceArtifactBuilderTest.java
+++ exonum-java-binding/testing/src/test/java/com/exonum/binding/test/runtime/ServiceArtifactBuilderTest.java
@@ -18,6 +18,7 @@ package com.exonum.binding.test.runtime;
 
 import static com.exonum.binding.test.runtime.ServiceArtifactBuilder.EXTENSIONS_INDEX_NAME;
 import static com.exonum.binding.test.runtime.ServiceArtifactBuilder.PLUGIN_ID_ATTRIBUTE_NAME;
+import static com.exonum.binding.test.runtime.ServiceArtifactBuilder.PLUGIN_VERSION_ATTRIBUTE_NAME;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -40,6 +41,7 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.JarInputStream;
 import java.util.jar.Manifest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.io.TempDir;
@@ -48,15 +50,20 @@ class ServiceArtifactBuilderTest {
 
   private static final String TEST_JAR_NAME = "test.jar";
 
+  private Path jarPath;
+
+  @BeforeEach
+  void setUp(@TempDir Path tempDir, TestReporter reporter) {
+    jarPath = tempDir.resolve(TEST_JAR_NAME);
+    reporter.publishEntry("test JAR path", jarPath.toString());
+  }
+
   @Test
-  void createArtifactSingleClass(@TempDir Path tempDir, TestReporter reporter) throws IOException {
-    Path jarPath = tempDir.resolve(TEST_JAR_NAME);
+  void createArtifactSingleClass() throws IOException {
     Class<TestService> testClass = TestService.class;
     new ServiceArtifactBuilder()
         .addClass(testClass)
         .writeTo(jarPath);
-
-    reporter.publishEntry("test JAR path", jarPath.toString());
 
     Map<String, byte[]> allJarEntries = readJarEntries(jarPath);
 
@@ -64,15 +71,11 @@ class ServiceArtifactBuilderTest {
   }
 
   @Test
-  void createArtifactSingleInnerClass(@TempDir Path tempDir, TestReporter reporter)
-      throws IOException {
-    Path jarPath = tempDir.resolve(TEST_JAR_NAME);
+  void createArtifactSingleInnerClass() throws IOException {
     Class<Inner> testClass = Inner.class;
     new ServiceArtifactBuilder()
         .addClass(testClass)
         .writeTo(jarPath);
-
-    reporter.publishEntry("test JAR path", jarPath.toString());
 
     Map<String, byte[]> allJarEntries = readJarEntries(jarPath);
 
@@ -81,15 +84,11 @@ class ServiceArtifactBuilderTest {
   }
 
   @Test
-  void createArtifactSeveralClasses(@TempDir Path tempDir, TestReporter reporter)
-      throws IOException {
-    Path jarPath = tempDir.resolve(TEST_JAR_NAME);
+  void createArtifactSeveralClasses() throws IOException {
     new ServiceArtifactBuilder()
         .addClass(ImmutableList.class)
         .addClasses(TestService.class, Bytes.class)
         .writeTo(jarPath);
-
-    reporter.publishEntry("test JAR path", jarPath.toString());
 
     Map<String, byte[]> allJarEntries = readJarEntries(jarPath);
 
@@ -109,13 +108,9 @@ class ServiceArtifactBuilderTest {
   }
 
   @Test
-  void createArtifactEmptyExtensions(@TempDir Path tempDir, TestReporter reporter)
-      throws IOException {
-    Path jarPath = tempDir.resolve(TEST_JAR_NAME);
+  void createArtifactEmptyExtensions() throws IOException {
     new ServiceArtifactBuilder()
         .writeTo(jarPath);
-
-    reporter.publishEntry("test JAR path", jarPath.toString());
 
     Map<String, byte[]> allJarEntries = readJarEntries(jarPath);
 
@@ -124,15 +119,11 @@ class ServiceArtifactBuilderTest {
   }
 
   @Test
-  void createArtifactSeveralExtensions(@TempDir Path tempDir, TestReporter reporter)
-      throws IOException {
-    Path jarPath = tempDir.resolve(TEST_JAR_NAME);
+  void createArtifactSeveralExtensions() throws IOException {
     new ServiceArtifactBuilder()
         .addExtensionEntry("e1")
         .addExtensionEntry("e2")
         .writeTo(jarPath);
-
-    reporter.publishEntry("test JAR path", jarPath.toString());
 
     Map<String, byte[]> allJarEntries = readJarEntries(jarPath);
 
@@ -144,21 +135,19 @@ class ServiceArtifactBuilderTest {
   }
 
   @Test
-  void createArtifactWithManifestFields(@TempDir Path tempDir, TestReporter reporter)
-      throws IOException {
-    Path jarPath = tempDir.resolve(TEST_JAR_NAME);
+  void createArtifactWithManifestFields() throws IOException {
     new ServiceArtifactBuilder()
-        .setManifestEntry("Exonum-Version", "3.1.0")
         .setPluginId("foo-service")
+        .setPluginVersion("1.0.2")
+        .setManifestEntry("Exonum-Version", "3.1.0")
         .writeTo(jarPath);
-
-    reporter.publishEntry("test JAR path", jarPath.toString());
 
     Manifest manifest = readJarManifest(jarPath);
 
     Attributes mainAttributes = manifest.getMainAttributes();
-    assertThat(mainAttributes.getValue("Exonum-Version")).isEqualTo("3.1.0");
     assertThat(mainAttributes.getValue(PLUGIN_ID_ATTRIBUTE_NAME)).isEqualTo("foo-service");
+    assertThat(mainAttributes.getValue(PLUGIN_VERSION_ATTRIBUTE_NAME)).isEqualTo("1.0.2");
+    assertThat(mainAttributes.getValue("Exonum-Version")).isEqualTo("3.1.0");
   }
 
   @SuppressWarnings("UnstableApiUsage")
