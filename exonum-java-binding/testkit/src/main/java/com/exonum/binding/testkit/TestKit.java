665956401599a414afdd0867eb0ae79b9a812839
Sergei Makarov (LCL) <makar93@mail.ru>
2019-04-08 22:59:22 -0400

    ECR-3050 Add TestKit P2 functionality

--- exonum-java-binding/testkit/src/main/java/com/exonum/binding/testkit/TestKit.java
+++ exonum-java-binding/testkit/src/main/java/com/exonum/binding/testkit/TestKit.java
@@ -23,20 +23,35 @@ import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 
 import com.exonum.binding.blockchain.Block;
-import com.exonum.binding.proxy.NativeHandle;
+import com.exonum.binding.blockchain.Blockchain;
+import com.exonum.binding.blockchain.serialization.BlockSerializer;
+import com.exonum.binding.common.blockchain.TransactionResult;
+import com.exonum.binding.common.hash.HashCode;
+import com.exonum.binding.common.message.TransactionMessage;
+import com.exonum.binding.common.serialization.Serializer;
+import com.exonum.binding.proxy.AbstractCloseableNativeProxy;
+import com.exonum.binding.proxy.Cleaner;
+import com.exonum.binding.proxy.CloseFailuresException;
 import com.exonum.binding.runtime.ReflectiveModuleSupplier;
 import com.exonum.binding.service.BlockCommittedEvent;
 import com.exonum.binding.service.Service;
 import com.exonum.binding.service.ServiceModule;
 import com.exonum.binding.service.adapters.UserServiceAdapter;
+import com.exonum.binding.storage.database.Snapshot;
+import com.exonum.binding.storage.indices.MapIndex;
+import com.exonum.binding.storage.indices.ProofMapIndexProxy;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import javax.annotation.Nullable;
 
@@ -54,41 +69,49 @@ import javax.annotation.Nullable;
  *
  * @see <a href="https://exonum.com/doc/version/0.10/get-started/test-service/">TestKit documentation</a>
  */
-public final class TestKit {
+public final class TestKit extends AbstractCloseableNativeProxy {
 
   @VisibleForTesting
   static final short MAX_SERVICE_NUMBER = 256;
-  private final Injector frameworkInjector = Guice.createInjector(new TestKitFrameworkModule());
+  private static final Serializer<Block> BLOCK_SERIALIZER = BlockSerializer.INSTANCE;
+  private static final Injector frameworkInjector =
+      Guice.createInjector(new TestKitFrameworkModule());
 
-  private final NativeHandle nativeHandle;
   private final Map<Short, Service> services = new HashMap<>();
 
-  private TestKit(List<Class<? extends ServiceModule>> serviceModules, EmulatedNodeType nodeType,
-                  short validatorCount, @Nullable TimeProvider timeProvider) {
+  private static TestKit newInstance(List<Class<? extends ServiceModule>> serviceModules,
+                                     EmulatedNodeType nodeType, short validatorCount,
+                                     @Nullable TimeProvider timeProvider) {
     List<UserServiceAdapter> serviceAdapters = toUserServiceAdapters(serviceModules);
-    populateServiceMap(serviceAdapters);
     boolean isAuditorNode = nodeType == EmulatedNodeType.AUDITOR;
     UserServiceAdapter[] userServiceAdapters = serviceAdapters.toArray(new UserServiceAdapter[0]);
     // TODO: fix after native implementation
-    nativeHandle = null;
-    //  nativeHandle = new NativeHandle(
-    //      nativeCreateTestKit(userServiceAdapters, isAuditorNode, validatorCount, timeProvider));
+    long nativeHandle = 0L;
+    //    long nativeHandle = nativeCreateTestKit(userServiceAdapters, isAuditorNode,
+    //    validatorCount, timeProvider);
+    return new TestKit(nativeHandle, serviceAdapters);
+  }
+
+  private TestKit(long nativeHandle, List<UserServiceAdapter> serviceAdapters) {
+    super(nativeHandle, true);
+    populateServiceMap(serviceAdapters);
   }
 
   /**
    * Returns a list of user service adapters created from given service modules.
    */
-  private List<UserServiceAdapter> toUserServiceAdapters(
+  private static List<UserServiceAdapter> toUserServiceAdapters(
       List<Class<? extends ServiceModule>> serviceModules) {
     return serviceModules.stream()
-        .map(this::createUserServiceAdapter)
+        .map(TestKit::createUserServiceAdapter)
         .collect(toList());
   }
 
   /**
    * Instantiates a service given its module and wraps in a UserServiceAdapter for the native code.
    */
-  private UserServiceAdapter createUserServiceAdapter(Class<? extends ServiceModule> moduleClass) {
+  private static UserServiceAdapter createUserServiceAdapter(
+      Class<? extends ServiceModule> moduleClass) {
     try {
       Supplier<ServiceModule> moduleSupplier = new ReflectiveModuleSupplier(moduleClass);
       ServiceModule serviceModule = moduleSupplier.get();
@@ -119,7 +142,7 @@ public final class TestKit {
    * Creates a TestKit network with a single validator node for a single service.
    */
   public static TestKit forService(Class<? extends ServiceModule> serviceModule) {
-    return new TestKit(singletonList(serviceModule), EmulatedNodeType.VALIDATOR, (short) 0, null);
+    return newInstance(singletonList(serviceModule), EmulatedNodeType.VALIDATOR, (short) 0, null);
   }
 
   /**
@@ -140,16 +163,117 @@ public final class TestKit {
     return serviceClass.cast(service);
   }
 
+  /**
+   * Creates a block with the given transaction. Transactions that are in the pool will be ignored.
+   *
+   * @return created block
+   * @see <a href="https://exonum.com/doc/version/0.10/advanced/consensus/specification/#pool-of-unconfirmed-transactions">Pool of Unconfirmed Transactions</a>
+   */
+  public Block createBlockWithTransactions(TransactionMessage transaction,
+                                           TransactionMessage... transactions) {
+    return createBlockWithTransactions(asList(transaction, transactions));
+  }
+
+  /**
+   * Creates a block with the given transactions. Transactions that are in the pool will be ignored.
+   *
+   * @return created block
+   * @see <a href="https://exonum.com/doc/version/0.10/advanced/consensus/specification/#pool-of-unconfirmed-transactions">Pool of Unconfirmed Transactions</a>
+   */
+  public Block createBlockWithTransactions(Iterable<TransactionMessage> transactions) {
+    List<TransactionMessage> messageList = Lists.newArrayList(transactions);
+    byte[][] transactionMessagesArr = messageList.stream()
+        .map(TransactionMessage::toBytes)
+        .toArray(byte[][]::new);
+    byte[] block = nativeCreateBlockWithTransactions(nativeHandle.get(), transactionMessagesArr);
+    return BLOCK_SERIALIZER.fromBytes(block);
+  }
+
+  /**
+   * Creates a block with the given transaction. Transactions that are in the pool will be ignored.
+   *
+   * @return created block
+   * @see <a href="https://exonum.com/doc/version/0.10/advanced/consensus/specification/#pool-of-unconfirmed-transactions">Pool of Unconfirmed Transactions</a>
+   */
+  public Block createBlockWithTransaction(TransactionMessage transaction) {
+    byte[][] transactionMessageArray = { transaction.toBytes() };
+    byte[] block = nativeCreateBlockWithTransactions(nativeHandle.get(), transactionMessageArray);
+    return BLOCK_SERIALIZER.fromBytes(block);
+  }
+
+  /**
+   * Creates a block with all transactions in the pool.
+   *
+   * @return created block
+   * @see <a href="https://exonum.com/doc/version/0.10/advanced/consensus/specification/#pool-of-unconfirmed-transactions">Pool of Unconfirmed Transactions</a>
+   */
+  public Block createBlock() {
+    byte[] block = nativeCreateBlock(nativeHandle.get());
+    return BLOCK_SERIALIZER.fromBytes(block);
+  }
+
+  /**
+   * Returns a list of in-pool transactions that match the given predicate.
+   *
+   * @see <a href="https://exonum.com/doc/version/0.10/advanced/consensus/specification/#pool-of-unconfirmed-transactions">Pool of Unconfirmed Transactions</a>
+   */
+  public List<TransactionMessage> findTransactionsInPool(Predicate<TransactionMessage> predicate) {
+    return withSnapshot((view) -> {
+      Blockchain blockchain = Blockchain.newInstance(view);
+      MapIndex<HashCode, TransactionMessage> txMessages = blockchain.getTxMessages();
+      List<TransactionMessage> messageList = ImmutableList.copyOf(txMessages.values());
+      // As only executed transactions are stored in TxResults, it wouldn't contain in-pool
+      // transactions
+      ProofMapIndexProxy<HashCode, TransactionResult> txResults = blockchain.getTxResults();
+      return messageList.stream()
+          .filter(predicate)
+          // TODO: is tx.hash() correct?
+          .filter(tx -> !txResults.containsKey(tx.hash()))
+          .collect(toList());
+    });
+  }
+
+  /**
+   * Performs a given function with a snapshot of the current database state.
+   *
+   * @param snapshotFunction a function to execute
+   * @param <ResultT> a type the function returns
+   * @return the result of applying the given function to the database state
+   */
+  public <ResultT> ResultT withSnapshot(Function<Snapshot, ResultT> snapshotFunction) {
+    try (Cleaner cleaner = new Cleaner("TestKit#withSnapshot")) {
+      long snapshotHandle = nativeCreateSnapshot(nativeHandle.get());
+      Snapshot snapshot = Snapshot.newInstance(snapshotHandle, cleaner);
+      return snapshotFunction.apply(snapshot);
+    } catch (CloseFailuresException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Returns the emulated TestKit node context.
+   */
+  public EmulatedNode getEmulatedNode() {
+    return nativeGetEmulatedNode(nativeHandle.get());
+  }
+
+  @Override
+  protected void disposeInternal() {
+    nativeFreeTestKit(nativeHandle.get());
+  }
+
   private native long nativeCreateTestKit(UserServiceAdapter[] services, boolean auditor,
       short withValidatorCount, TimeProvider timeProvider);
 
   private native long nativeCreateSnapshot(long nativeHandle);
 
-  private native Block nativeCreateBlock(long nativeHandle);
+  private native byte[] nativeCreateBlock(long nativeHandle);
 
-  private native Block nativeCreateBlockWithTransactions(long nativeHandle, byte[][] transactions);
+  private native byte[] nativeCreateBlockWithTransactions(long nativeHandle, byte[][] transactions);
 
   private native EmulatedNode nativeGetEmulatedNode(long nativeHandle);
+
+  private native void nativeFreeTestKit(long nativeHandle);
 
   /**
    * Creates a new builder for the TestKit.
@@ -230,7 +354,7 @@ public final class TestKit {
      */
     public final TestKit build() {
       checkCorrectServiceNumber(services.size());
-      return new TestKit(services, nodeType, validatorCount, timeProvider);
+      return newInstance(services, nodeType, validatorCount, timeProvider);
     }
 
     private void checkCorrectServiceNumber(int serviceCount) {
