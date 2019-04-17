296b4f6921a4c3c09031271646acaf55c7ed055a
Sergey Kletsun <sergey.kletsun@gmail.com>
2019-04-17 18:05:12 +0300

    Extend native integration tests for service runtime [ECR-3011]. (#814)
    * Integration tests for service runtime are extended with fake service and testing utils for artifacts generation.
    
    * It is possible to create instance of JavaServiceRuntime for a given JVM now.

---
+++ exonum-java-binding/core/rust/integration_tests/tests/service_runtime_bootstrap.rs
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 The Exonum Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+extern crate integration_tests;
+extern crate java_bindings;
+
+use integration_tests::vm::get_fakes_classpath;
+use java_bindings::{
+    Config, InternalConfig, JavaServiceRuntime, JniExecutor, JvmConfig, RuntimeConfig,
+};
+
+#[test]
+fn bootstrap() {
+    let jvm_config = JvmConfig {
+        args_prepend: vec![],
+        args_append: vec![],
+        jvm_debug_socket: None,
+    };
+
+    let runtime_config = RuntimeConfig {
+        log_config_path: "".to_owned(),
+        port: 6300,
+    };
+
+    let config = Config {
+        jvm_config,
+        runtime_config,
+    };
+
+    let internal_config = InternalConfig {
+        system_class_path: get_fakes_classpath(),
+        system_lib_path: None,
+    };
+
+    let runtime = JavaServiceRuntime::new(config, internal_config);
+
+    let result = runtime
+        .get_executor()
+        .with_attached(|env| env.get_version());
+
+    assert!(result.is_ok());
+    assert!(i32::from(result.unwrap()) > 0);
+}
