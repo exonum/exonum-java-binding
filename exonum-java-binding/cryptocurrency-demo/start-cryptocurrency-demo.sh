1dfd3f0761b1a1d073f216079e750bebc23cf502
Sergey Kletsun <sergey.kletsun@gmail.com>
2019-04-17 09:55:59 +0300

    Script is rewritten in order to not rely on the `EXONUM_HOME` environment variable.

--- exonum-java-binding/cryptocurrency-demo/start-cryptocurrency-demo.sh
+++ exonum-java-binding/cryptocurrency-demo/start-cryptocurrency-demo.sh
@@ -13,43 +13,37 @@ set -eu -o pipefail
 
 header "DETECTING ENVIRONMENT"
 
-# EXONUM_HOME shoudl be set to the root directory of Exonum Java App installed
-if [ -z "${EXONUM_HOME+x}" ];
+# Check whether the Exonum Java App is installed.
+EXONUM_JAVA_APP="exonum-java"
+command -v ${EXONUM_JAVA_APP} >/dev/null 2>&1 || { echo >&2 "Please install the Exonum Java App and make sure that 'exonum-java' binary is available via PATH. Aborting."; exit 1; }
+
+# Check that path to the JVM library is set. We cannot just set the LD_LIBRARY_PATH outside the script because of OSX SIP limitation.
+if [ -z "${LIB_JVM_PATH+x}" ];
 then
-    echo "ERROR: EXONUM_HOME variable is empty. Please point the EXONUM_HOME to the root of installed Exonum Java App."
+    echo "ERROR: LIB_JVM_PATH variable is not set. Please make sure it points to the directory where the JVM dynamic library (libjvm.[so|dylib]) resides."
     exit 1
 fi
-echo "EXONUM_HOME=${EXONUM_HOME}"
 
-EXONUM_JAVA_APP="${EXONUM_HOME}/exonum-java"
+export LD_LIBRARY_PATH=${LIB_JVM_PATH}
+echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
 
 # Find the latest version of artifact and build if not found.
 ARTIFACT_PATH="$(find ./target -type f -name exonum-java-binding-cryptocurrency-demo-*-artifact.jar)"
 
-if [ -z "${ARTIFACT_PATH}+x" ];
+if [ -z "${ARTIFACT_PATH}" ];
 then
-    mvn install
+    mvn package
     ARTIFACT_PATH="$(find ./target -type f -name exonum-java-binding-cryptocurrency-demo-*-artifact.jar)"
 fi
 echo "ARTIFACT_PATH=${ARTIFACT_PATH}"
 
-# Prepare the services configuration file
+# Prepare the services configuration file.
 SERVICES_CONFIG_FILE="services.toml"
 SERVICE_NAME="cryptocurrency-demo"
 echo "[user_services]" > ${SERVICES_CONFIG_FILE}
 echo "${SERVICE_NAME} = '${ARTIFACT_PATH}'" >> ${SERVICES_CONFIG_FILE}
 
-#TODO: Can be removed after ECR-3030
-EJB_LOG_CONFIG_PATH="${EXONUM_HOME}/log4j-fallback.xml"
-echo EJB_LOG_CONFIG_PATH=${EJB_LOG_CONFIG_PATH}
-
-# Find the directory containing libjvm.
-JAVA_HOME="$(java -XshowSettings:properties -version 2>&1 > /dev/null | grep 'java.home' | awk '{print $3}')"
-JAVA_LIB_DIR="$(find ${JAVA_HOME} -type f -name libjvm.\* | xargs -n1 dirname)"
-export LD_LIBRARY_PATH="$JAVA_LIB_DIR"
-echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
-
-# Clear test dir
+# Clear test dir.
 rm -rf testnet
 mkdir testnet
 
@@ -72,5 +66,4 @@ ${EXONUM_JAVA_APP} run -d testnet/db -c testnet/node.toml \
  --consensus-key-pass pass \
  --service-key-pass pass \
  --public-api-address 127.0.0.1:3000 \
- --ejb-log-config-path $EJB_LOG_CONFIG_PATH \
  --ejb-port 6000
