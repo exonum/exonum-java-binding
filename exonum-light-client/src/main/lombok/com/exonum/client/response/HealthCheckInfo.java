e1e9c8873f95e62a9afa689d884709a7a197b3f8
Oleg Bondar <bullet-tooth@users.noreply.github.com>
2019-05-22 11:55:25 +0300

    fix doc comments

--- exonum-light-client/src/main/lombok/com/exonum/client/response/HealthCheckInfo.java
+++ exonum-light-client/src/main/lombok/com/exonum/client/response/HealthCheckInfo.java
@@ -22,7 +22,8 @@ import lombok.Value;
 @Value
 public class HealthCheckInfo {
   /**
-   * Shows information about whether it is possible to achieve the consensus between
+   * Consensus status which shows an information about
+   * whether it is possible to achieve the consensus between
    * validators in the current state.
    */
   ConsensusStatus consensusStatus;
