4dc5d2d6075a2d42c516697108025181bd45c95f
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2019-06-13 13:38:50 +0300

    Extend the script

--- exonum-java-binding/profile_tests.sh
+++ exonum-java-binding/profile_tests.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
+## No parallel — our baseline
+mvn clean test-compile -q -P ci-build
+mvn install -o -P ci-build -Dprofile="No opts/SeqBuild/SeqTests" -DrunJavaTestsInParallel=false
+
 ## Parallel
-
+### No extra options
+#### Seq build
 mvn clean test-compile -q -P ci-build
-mvn install -o -P ci-build -DrunJavaTestsInParallel=true -Dprofile
+mvn install -o -P ci-build -Dprofile="No opts/SeqBuild/ParTests" -DrunJavaTestsInParallel=true
 
-## No parallel
+#### Parallel build
 mvn clean test-compile -q -P ci-build
-mvn install -o -P ci-build -DrunJavaTestsInParallel=false -Dprofile
+mvn install -o -P ci-build -Dprofile="No opts/ParBuild/ParTests" -DrunJavaTestsInParallel=true -T 1C
+
+### Extra options
+export MAVEN_OPTS='-XX:+UseParallelGC'
+mvn clean test-compile -q -P ci-build
+mvn install -o -P ci-build -Dprofile="Options=${MAVEN_OPTS}/ParBuild/ParTests" -DrunJavaTestsInParallel=true -T 1C
+
+export MAVEN_OPTS='-XX:TieredStopAtLevel=1'
+mvn clean test-compile -q -P ci-build
+mvn install -o -P ci-build -Dprofile="Options=${MAVEN_OPTS}/ParBuild/ParTests" -DrunJavaTestsInParallel=true -T 1C
+
+export MAVEN_OPTS='-XX:TieredStopAtLevel=1 -XX:+UseParallelGC'
+mvn clean test-compile -q -P ci-build
+mvn install -o -P ci-build -Dprofile="Options=${MAVEN_OPTS}/ParBuild/ParTests" -DrunJavaTestsInParallel=true -T 1C
