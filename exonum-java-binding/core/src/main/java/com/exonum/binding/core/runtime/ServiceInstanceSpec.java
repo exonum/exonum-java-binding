7432bb58af6a3a63f0435408245ba9861cc0cd59
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2019-08-05 09:37:29 +0300

    Clarify the service instance Javadoc

--- exonum-java-binding/core/src/main/java/com/exonum/binding/core/runtime/ServiceInstanceSpec.java
+++ exonum-java-binding/core/src/main/java/com/exonum/binding/core/runtime/ServiceInstanceSpec.java
@@ -12,23 +12,22 @@ import io.vertx.ext.web.Router;
 public abstract class ServiceInstanceSpec {
 
   /**
-   * Returns the service artifact id.
-   */
-  public abstract ServiceArtifactId getArtifactId();
-
-  /**
-   * Returns the name of the service instance. It serves as the unique string identifier
-   * of this service, e.g., to route the requests to its public API.
-   *
-   * @see com.exonum.binding.core.service.Service#createPublicApiHandlers(Node, Router)
+   * Returns the name of the service instance. It serves as the primary identifier of this service
+   * in most operations. It is assigned by the network administrators.
    */
   public abstract String getName();
 
   /**
-   * Returns the numeric id of the service instance. It is mainly used to route the transaction
-   * messages belonging to this instance.
+   * Returns the numeric id of the service instance. Exonum assigns it to the service
+   * on instantiation. It is mainly used to route the transaction messages belonging
+   * to this instance.
    *
    * @see TransactionMessage#getServiceId()
    */
   public abstract int getId();
+
+  /**
+   * Returns the service artifact id.
+   */
+  public abstract ServiceArtifactId getArtifactId();
 }
