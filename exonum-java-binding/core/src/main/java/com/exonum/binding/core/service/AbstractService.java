4e7864df5664bb8cba40079d27df6549e496f06d
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2019-08-05 16:00:53 +0300

    Update the Service interface:
    * Remove:
      - getId — this one might be reconsidered
      - getName
    
    * Update configure to take configuration parameters and return void
    
    * Make getStateHashes implementation required (as no state hashes
    usually means "you don't need the blockchain or do it wrong").
    
    Obviously, it neither compiles nor works.

--- exonum-java-binding/core/src/main/java/com/exonum/binding/core/service/AbstractService.java
+++ exonum-java-binding/core/src/main/java/com/exonum/binding/core/service/AbstractService.java
@@ -16,54 +16,15 @@
 
 package com.exonum.binding.core.service;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import com.exonum.binding.common.hash.HashCode;
 import com.exonum.binding.core.storage.database.Snapshot;
 import com.exonum.binding.core.storage.database.View;
-import com.exonum.binding.core.transaction.RawTransaction;
-import com.exonum.binding.core.transaction.Transaction;
 import java.util.List;
 
 /**
  * A base class for user services.
  */
 public abstract class AbstractService implements Service {
-
-  private final short id;
-  private final String name;
-  private final TransactionConverter transactionConverter;
-
-  /**
-   * Creates an AbstractService.
-   *
-   * @param id an id of the service
-   * @param name a name of the service
-   * @param transactionConverter a transaction converter that is aware of
-   *                             all transactions of this service
-   */
-  protected AbstractService(short id, String name, TransactionConverter transactionConverter) {
-    this.id = id;
-    checkArgument(!name.isEmpty(), "The service name must not be empty");
-    this.name = name;
-    this.transactionConverter = checkNotNull(transactionConverter);
-  }
-
-  @Override
-  public short getId() {
-    return id;
-  }
-
-  @Override
-  public String getName() {
-    return name;
-  }
-
-  @Override
-  public Transaction convertToTransaction(RawTransaction rawTransaction) {
-    return transactionConverter.toTransaction(rawTransaction);
-  }
 
   @Override
   public List<HashCode> getStateHashes(Snapshot snapshot) {
