2504f5257a06520650158322386574ff48aa85b9
Sergey Kletsun <sergey.kletsun@gmail.com>
2019-08-15 02:48:07 +0300

    Pointed dependencies to latest exonum v0.12.

--- exonum-java-binding/core/rust/exonum-java/src/main.rs
+++ exonum-java-binding/core/rust/exonum-java/src/main.rs
@@ -15,7 +15,7 @@
  */
 
 extern crate env_logger;
-//extern crate exonum_btc_anchoring; // FIXME: uncomment when anchoring uses merkledb
+extern crate exonum_btc_anchoring;
 extern crate exonum_configuration;
 extern crate exonum_time;
 extern crate java_bindings;
