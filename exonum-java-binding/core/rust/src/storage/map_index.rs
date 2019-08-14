6ed2197b35d7aae62c3d30c14bb74a117cb0fe51
Sergey Kletsun <sergey.kletsun@gmail.com>
2019-08-15 02:23:34 +0300

    Remove JNI methods for new methods of indexes [ECR-3317] (#1041). This reverts commit 4ff2963c.

--- exonum-java-binding/core/rust/src/storage/map_index.rs
+++ exonum-java-binding/core/rust/src/storage/map_index.rs
@@ -18,7 +18,7 @@ use exonum_merkledb::{
 };
 use jni::{
     objects::{JClass, JObject, JString},
-    sys::{jboolean, jbyteArray, jlong, jobject},
+    sys::{jboolean, jbyteArray, jobject},
     JNIEnv,
 };
 
@@ -312,38 +312,6 @@ pub extern "system" fn Java_com_exonum_binding_core_storage_indices_MapIndexProx
             map.clear();
             Ok(())
         }
-    });
-    utils::unwrap_exc_or_default(&env, res)
-}
-
-/// Returns `true` if the map has no entries.
-#[no_mangle]
-pub extern "system" fn Java_com_exonum_binding_core_storage_indices_MapIndexProxy_nativeIsEmpty(
-    env: JNIEnv,
-    _: JObject,
-    map_handle: Handle,
-) -> jboolean {
-    let res = panic::catch_unwind(|| {
-        Ok(match *handle::cast_handle::<IndexType>(map_handle) {
-            IndexType::SnapshotIndex(ref map) => map.is_empty(),
-            IndexType::ForkIndex(ref map) => map.is_empty(),
-        } as jboolean)
-    });
-    utils::unwrap_exc_or_default(&env, res)
-}
-
-/// Returns the number of entries in the map.
-#[no_mangle]
-pub extern "system" fn Java_com_exonum_binding_core_storage_indices_MapIndexProxy_nativeSize(
-    env: JNIEnv,
-    _: JObject,
-    map_handle: Handle,
-) -> jlong {
-    let res = panic::catch_unwind(|| {
-        Ok(match *handle::cast_handle::<IndexType>(map_handle) {
-            IndexType::SnapshotIndex(ref map) => map.len(),
-            IndexType::ForkIndex(ref map) => map.len(),
-        } as jlong)
     });
     utils::unwrap_exc_or_default(&env, res)
 }
