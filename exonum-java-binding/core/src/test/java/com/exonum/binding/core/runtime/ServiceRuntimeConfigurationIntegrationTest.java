b4c240c0c59b0f46c606a0cffce4b41fedde87e1
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2019-10-21 18:56:33 +0300

    Fix the runtime module:
    Add a missing binding for ServicesFactory in the runtime module.
    Add an integration test for the runtime configuration.

---
+++ exonum-java-binding/core/src/test/java/com/exonum/binding/core/runtime/ServiceRuntimeConfigurationIntegrationTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2019 The Exonum Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.exonum.binding.core.runtime;
+
+import static java.util.Collections.emptyMap;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.exonum.binding.test.RequiresNativeLibrary;
+import com.exonum.binding.test.runtime.ServiceArtifactBuilder;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Stage;
+import java.io.IOException;
+import java.nio.file.Path;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+/**
+ * An integration test of the runtime: the runtime module bindings and ServiceRuntime integration
+ * with its dependencies.
+ */
+@RequiresNativeLibrary
+class ServiceRuntimeConfigurationIntegrationTest {
+
+  private static final ServiceArtifactId ARTIFACT_ID =
+      ServiceArtifactId.parseFrom("com.exonum.binding:test-service:1.0.0");
+  private static final String ARTIFACT_FILENAME = "test-service.jar";
+
+  @BeforeEach
+  void createValidArtifact(@TempDir Path tmpArtifactDir) throws IOException {
+    Class<?> serviceModule = TestServiceModule.class;
+    Path artifactLocation = tmpArtifactDir.resolve(ARTIFACT_FILENAME);
+    new ServiceArtifactBuilder()
+        .setPluginId(ARTIFACT_ID.toString())
+        .setPluginVersion(ARTIFACT_ID.getVersion())
+        .addClasses(TestService.class)
+        .addExtensionClass(serviceModule)
+        .writeTo(artifactLocation);
+  }
+
+  @Test
+  void runtimeConfigurationTest(@TempDir Path tmpArtifactDir) throws Exception {
+    int port = 0; // any port
+    // Use 'production' stage as it involves up-front error checking of the configured bindings
+    Injector injector = Guice.createInjector(Stage.PRODUCTION,
+        new FrameworkModule(tmpArtifactDir, port, emptyMap()));
+
+    // Create the runtime
+    ServiceRuntime runtime = injector.getInstance(ServiceRuntime.class);
+
+    // Deploy the service to the runtime
+    runtime.deployArtifact(ARTIFACT_ID, ARTIFACT_FILENAME);
+    assertTrue(runtime.isArtifactDeployed(ARTIFACT_ID));
+
+    // Create a service instance
+    String name = "s1";
+    ServiceInstanceSpec instanceSpec = ServiceInstanceSpec.newInstance(name, 1, ARTIFACT_ID);
+    runtime.createService(instanceSpec);
+
+    assertThat(runtime.findService(name)).isNotEmpty();
+  }
+}
