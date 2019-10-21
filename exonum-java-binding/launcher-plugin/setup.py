5bd08038197bc29218b5e661393f0c57e34086ac
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-10-21 18:04:31 +0300

    Use maven to compile protobuf

--- exonum-java-binding/launcher-plugin/setup.py
+++ exonum-java-binding/launcher-plugin/setup.py
@@ -15,63 +15,6 @@
 # limitations under the License.
 
 from distutils.core import setup
-from distutils.command.build_py import build_py
-from distutils.command.clean import clean
-from distutils.spawn import find_executable
-from distutils.debug import DEBUG
-
-import os
-import sys
-import subprocess
-
-protobuf_source = "exonum_java_runtime_plugin/proto/service_runtime.proto"
-
-if 'PROTOC' in os.environ and os.path.exists(os.environ['PROTOC']):
-    protoc = os.environ['PROTOC']
-else:
-    protoc = find_executable('protoc')
-
-
-def generate_proto(source):
-    """Invoke Protocol Compiler to generate python from given source .proto."""
-    if not os.path.exists(source):
-        sys.stderr.write('Can\'t find required file: %s\n' % source)
-        sys.exit(1)
-
-    output = source.replace('.proto', '_pb2.py')
-    if (not os.path.exists(output) or
-            (os.path.getmtime(source) > os.path.getmtime(output))):
-        if DEBUG:
-            print('Generating %s' % output)
-
-        if protoc is None:
-            sys.stderr.write(
-                'protoc not found. Is protobuf-compiler installed? \n'
-                'Alternatively, you can point the PROTOC environment variable at a '
-                'local version.')
-            sys.exit(1)
-
-        protoc_command = [protoc, '-I.', '--python_out=.', source]
-        if subprocess.call(protoc_command) != 0:
-            sys.exit(1)
-
-
-class BuildPy(build_py):
-    def run(self):
-        generate_proto(protobuf_source)
-        build_py.run(self)
-
-
-class Clean(clean):
-    def run(self):
-        # Delete generated files in the code tree.
-        for (dirpath, dirnames, filenames) in os.walk("."):
-            for filename in filenames:
-                filepath = os.path.join(dirpath, filename)
-                if filepath.endswith("_pb2.py"):
-                    os.remove(filepath)
-        clean.run(self)
-
 
 install_requires = []  # Since `exonum-launcher` is not on pypi yet, suppose we have it pre-installed.
 
@@ -85,8 +28,4 @@ setup(
     packages=["exonum_java_runtime_plugin"],
     install_requires=install_requires,
     python_requires=python_requires,
-    cmdclass={
-        'build_py': BuildPy,
-        'clean': Clean,
-    }
 )
