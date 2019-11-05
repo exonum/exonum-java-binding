37d6840f3f166e6307889adde44203dea3c76fb3
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-11-05 14:51:26 +0300

    Fix comments in build.rs

--- exonum-java-binding/core/rust/build.rs
+++ exonum-java-binding/core/rust/build.rs
@@ -5,12 +5,13 @@ use exonum_build::ProtobufGenerator;
 use std::env::var_os;
 
 fn main() {
-    // We need to link to libc++.dylib on Mac if using static linkage with RocksDB
-    // This is because `librocksdb.a` provided with Homebrew package depends on
-    // `libc++.dylib` and in case of static linkage the resulting `libjava_bindigs.dylib`
-    // also needs to be linked against `libc++.dylib`.
     if cfg!(target_os = "macos") && var_os("ROCKSDB_STATIC").is_some() {
+        // We need to link to libc++.dylib on Mac if using static linkage with RocksDB
+        // This is because `librocksdb.a` provided with Homebrew package depends on
+        // `libc++.dylib` and in case of static linkage the resulting `libjava_bindigs.dylib`
+        // also needs to be linked against `libc++.dylib`.
         println!("cargo:rustc-link-lib=dylib=c++");
+        // We do the same thing for zstd.a as official rocksdb package depends on this library.
         println!("cargo:rustc-link-lib=static=zstd");
     }
 
