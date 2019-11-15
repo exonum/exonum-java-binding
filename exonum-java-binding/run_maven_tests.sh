f60d3362de9ac9acd5c1a338a415b52e31601a84
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2019-11-15 17:11:09 +0200

    Add BOM installation to scripts

--- exonum-java-binding/run_maven_tests.sh
+++ exonum-java-binding/run_maven_tests.sh
@@ -18,4 +18,4 @@ mvn install \
   --activate-profiles ci-build \
   -Drust.compiler.version="${RUST_COMPILER_VERSION}" \
   `# Keep the list of active services in sync with run_all_tests.sh in the root` \
-  -pl core,fake-service,integration-tests,cryptocurrency-demo,qa-service -am
+  -pl core,fake-service,integration-tests,cryptocurrency-demo,qa-service,bom -am
