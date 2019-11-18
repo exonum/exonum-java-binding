324646f201a471d1aa06ad311c9d701546b11aff
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-11-18 11:12:06 +0300

    Use 1 as field number in test protobuf messages

--- exonum-java-binding/launcher-plugins/tests/test_instance_plugin.py
+++ exonum-java-binding/launcher-plugins/tests/test_instance_plugin.py
@@ -23,8 +23,7 @@ class TestInstancePlugin(unittest.TestCase):
 
         for instance in config.instances:
             spec = instance_loader.load_spec(None, instance)
-            # https://developers.google.com/protocol-buffers/docs/encoding#strings
-            self.assertEqual(spec, b"\x12\x07\x74\x65\x73\x74\x69\x6e\x67")
+            self.assertEqual(spec, b"\n\x07\x74\x65\x73\x74\x69\x6e\x67")
 
     def test_plugin_custom_message_name(self) -> None:
         config = self.load_config("custom_message_name.yml")
@@ -33,8 +32,7 @@ class TestInstancePlugin(unittest.TestCase):
 
         for instance in config.instances:
             spec = instance_loader.load_spec(None, instance)
-            # https://developers.google.com/protocol-buffers/docs/encoding#strings
-            self.assertEqual(spec, b"\x12\x07\x74\x65\x73\x74\x69\x6e\x67")
+            self.assertEqual(spec, b"\n\x07\x74\x65\x73\x74\x69\x6e\x67")
 
     def test_plugin_errors_no_config_field(self) -> None:
         config = self.load_config("no_config.yml")
