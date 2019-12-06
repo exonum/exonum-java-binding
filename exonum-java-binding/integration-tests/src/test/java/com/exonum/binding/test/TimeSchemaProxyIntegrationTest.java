1ef34028f840001fd2753a9ac1f71bb871c52d45
Sergei Makarov (LCL) <makar93@mail.ru>
2019-12-06 16:06:23 -0500

    Remove unused import

--- exonum-java-binding/integration-tests/src/test/java/com/exonum/binding/test/TimeSchemaProxyIntegrationTest.java
+++ exonum-java-binding/integration-tests/src/test/java/com/exonum/binding/test/TimeSchemaProxyIntegrationTest.java
@@ -40,7 +40,6 @@ import java.util.Optional;
 import java.util.function.Consumer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
