e6bb48f889fe3403c9c844f330d6fe425c64664c
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2020-01-29 16:25:24 +0300

    Use GenericAccess in entry.rs

--- exonum-java-binding/core/rust/src/storage/entry.rs
+++ exonum-java-binding/core/rust/src/storage/entry.rs
@@ -12,7 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use exonum_merkledb::{access::FromAccess, Entry, Fork, Snapshot};
+use exonum_merkledb::{
+    access::AccessExt,
+    generic::{ErasedAccess, GenericRawAccess},
+    Entry,
+};
 use jni::{
     objects::{JClass, JObject, JString},
     sys::{jboolean, jbyteArray},
@@ -22,36 +26,24 @@ use jni::{
 use std::{panic, ptr};
 
 use handle::{self, Handle};
-use storage::db::{Value, View, ViewRef};
+use storage::db::Value;
 use utils;
 
-type Index<T> = Entry<T, Value>;
-
-enum IndexType {
-    SnapshotIndex(Index<&'static dyn Snapshot>),
-    ForkIndex(Index<&'static Fork>),
-}
+type Index = Entry<GenericRawAccess<'static>, Value>;
 
 /// Returns pointer to the created `Entry` object.
 #[no_mangle]
 pub extern "system" fn Java_com_exonum_binding_core_storage_indices_EntryIndexProxy_nativeCreate(
     env: JNIEnv,
     _: JClass,
-    name: JString,
+    address: JString,
     view_handle: Handle,
 ) -> Handle {
     let res = panic::catch_unwind(|| {
-        let name = utils::convert_to_string(&env, name)?;
-        Ok(handle::to_handle(
-            match handle::cast_handle::<View>(view_handle).get() {
-                ViewRef::Snapshot(snapshot) => {
-                    IndexType::SnapshotIndex(Index::from_access(snapshot, name.into()).unwrap())
-                }
-                ViewRef::Fork(fork) => {
-                    IndexType::ForkIndex(Index::from_access(fork, name.into()).unwrap())
-                }
-            },
-        ))
+        let address = utils::convert_to_string(&env, address)?;
+        let access = handle::cast_handle::<ErasedAccess>(view_handle);
+        let index: Index = access.get_entry(address);
+        Ok(handle::to_handle(index))
     });
     utils::unwrap_exc_or_default(&env, res)
 }
@@ -63,7 +55,7 @@ pub extern "system" fn Java_com_exonum_binding_core_storage_indices_EntryIndexPr
     _: JClass,
     entry_handle: Handle,
 ) {
-    handle::drop_handle::<IndexType>(&env, entry_handle);
+    handle::drop_handle::<Index>(&env, entry_handle);
 }
 
 /// Returns the value or null pointer if it is absent.
@@ -74,11 +66,9 @@ pub extern "system" fn Java_com_exonum_binding_core_storage_indices_EntryIndexPr
     entry_handle: Handle,
 ) -> jbyteArray {
     let res = panic::catch_unwind(|| {
-        let val = match *handle::cast_handle::<IndexType>(entry_handle) {
-            IndexType::SnapshotIndex(ref entry) => entry.get(),
-            IndexType::ForkIndex(ref entry) => entry.get(),
-        };
-        match val {
+        let index = handle::cast_handle::<Index>(entry_handle);
+        let value = index.get();
+        match value {
             Some(val) => env.byte_array_from_slice(&val),
             None => Ok(ptr::null_mut()),
         }
@@ -94,10 +84,9 @@ pub extern "C" fn Java_com_exonum_binding_core_storage_indices_EntryIndexProxy_n
     entry_handle: Handle,
 ) -> jboolean {
     let res = panic::catch_unwind(|| {
-        Ok(match *handle::cast_handle::<IndexType>(entry_handle) {
-            IndexType::SnapshotIndex(ref entry) => entry.exists(),
-            IndexType::ForkIndex(ref entry) => entry.exists(),
-        } as jboolean)
+        let index = handle::cast_handle::<Index>(entry_handle);
+        let exists = index.exists();
+        Ok(exists as jboolean)
     });
     utils::unwrap_exc_or_default(&env, res)
 }
@@ -110,15 +99,11 @@ pub extern "system" fn Java_com_exonum_binding_core_storage_indices_EntryIndexPr
     entry_handle: Handle,
     value: jbyteArray,
 ) {
-    let res = panic::catch_unwind(|| match *handle::cast_handle::<IndexType>(entry_handle) {
-        IndexType::SnapshotIndex(_) => {
-            panic!("Unable to modify snapshot.");
-        }
-        IndexType::ForkIndex(ref mut entry) => {
-            let value = env.convert_byte_array(value)?;
-            entry.set(value);
-            Ok(())
-        }
+    let res = panic::catch_unwind(|| {
+        let index = handle::cast_handle::<Index>(entry_handle);
+        let value = env.convert_byte_array(value)?;
+        index.set(value);
+        Ok(())
     });
     utils::unwrap_exc_or_default(&env, res)
 }
@@ -130,14 +115,10 @@ pub extern "C" fn Java_com_exonum_binding_core_storage_indices_EntryIndexProxy_n
     _: JObject,
     entry_handle: Handle,
 ) {
-    let res = panic::catch_unwind(|| match *handle::cast_handle::<IndexType>(entry_handle) {
-        IndexType::SnapshotIndex(_) => {
-            panic!("Unable to modify snapshot.");
-        }
-        IndexType::ForkIndex(ref mut entry) => {
-            entry.remove();
-            Ok(())
-        }
+    let res = panic::catch_unwind(|| {
+        let index = handle::cast_handle::<Index>(entry_handle);
+        index.remove();
+        Ok(())
     });
     utils::unwrap_exc_or_default(&env, res)
 }
