c57a96448a93bed2eb22a84ed35c2ff1d9f12c8c
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2020-03-04 12:09:52 +0200

    Step 2a-2: Add an implementation of AddVehicle tx

--- exonum-java-binding/tutorials/car-registry/car-registry-service/src/main/java/com/example/car/MyService.java
+++ exonum-java-binding/tutorials/car-registry/car-registry-service/src/main/java/com/example/car/MyService.java
@@ -16,13 +16,22 @@
 
 package com.example.car;
 
+import com.example.car.messages.Transactions;
+import com.example.car.messages.VehicleOuterClass.Vehicle;
 import com.exonum.binding.core.runtime.ServiceInstanceSpec;
 import com.exonum.binding.core.service.AbstractService;
 import com.exonum.binding.core.service.Node;
+import com.exonum.binding.core.storage.indices.ProofMapIndexProxy;
+import com.exonum.binding.core.transaction.ExecutionException;
+import com.exonum.binding.core.transaction.Transaction;
+import com.exonum.binding.core.transaction.TransactionContext;
 import com.google.inject.Inject;
 import io.vertx.ext.web.Router;
 
 public final class MyService extends AbstractService {
+
+  public static final int ADD_VEHICLE_TX_ID = 0;
+  public static final byte ID_ALREADY_EXISTS_ERROR_CODE = 100;
 
   @Inject
   public MyService(ServiceInstanceSpec instanceSpec) {
@@ -31,4 +40,26 @@ public final class MyService extends AbstractService {
 
   @Override
   public void createPublicApiHandlers(Node node, Router router) {}
+
+  // ci-block ci-add-vehicle {
+  @Transaction(ADD_VEHICLE_TX_ID)
+  public void addVehicle(Transactions.AddVehicle args, TransactionContext context) {
+    var serviceData = context.getServiceData();
+    var schema = new MySchema(serviceData);
+    ProofMapIndexProxy<String, Vehicle> vehicles = schema.vehicles();
+
+    // Check there is no vehicle with such id in the registry already
+    var newVehicle = args.getNewVehicle();
+    var id = newVehicle.getId();
+    if (vehicles.containsKey(id)) {
+      var existingVehicle = vehicles.get(id);
+      var errorDescription = String.format("The registry already contains a vehicle "
+          + "with id (%s): existing=%s, new=%s", id, existingVehicle, newVehicle);
+      throw new ExecutionException(ID_ALREADY_EXISTS_ERROR_CODE, errorDescription);
+    }
+
+    // Add the vehicle to the registry
+    vehicles.put(id, newVehicle);
+  }
+  // }
 }
